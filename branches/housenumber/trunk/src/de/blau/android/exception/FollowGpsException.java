90571b51289f3faf71cccd688b59beb50c3a9129
mattelacchiato <mattelacchiato@621d1362-f450-11dd-a7be-4b3ef3b0700c>
2009-03-17 16:40:43 +0000

    Issue 18:  	 Start of housenumber support Applied Daniel's patch.

---
+++ branches/housenumber/trunk/src/de/blau/android/exception/FollowGpsException.java
@@ -0,0 +1,14 @@
+package de.blau.android.exception;
+
+public class FollowGpsException extends OsmException {
+
+	public FollowGpsException(final String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1379741429500117963L;
+
+}
