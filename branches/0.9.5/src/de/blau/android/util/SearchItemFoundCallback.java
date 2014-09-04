e2edfa63b3d009b3eb0f2b016494f41326dec491
sp8962@gmail.com <sp8962@gmail.com@621d1362-f450-11dd-a7be-4b3ef3b0700c>
2014-09-04 10:29:55 +0000

    Remove re-integrated branch

--- branches/0.9.5/src/de/blau/android/util/SearchItemFoundCallback.java
+++
@@ -1,8 +0,0 @@
-package de.blau.android.util;
-
-import de.blau.android.util.Search.SearchResult;
-
-public abstract class SearchItemFoundCallback {
-	abstract public void onItemFound(SearchResult sr);
-}
-
