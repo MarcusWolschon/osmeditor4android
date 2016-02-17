4c53aa62152312291e98f14e889ae69ad769b9a4
Tobias Preuss <tobias.preuss@googlemail.com>
2016-02-17 10:37:59 +0100

    Replace deprecated HttpURLConnection with OkHttpClient.

--- src/de/blau/android/util/Search.java
+++ src/de/blau/android/util/Search.java
@@ -1,10 +1,9 @@
 package de.blau.android.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -29,6 +28,10 @@ import de.blau.android.R;
 import de.blau.android.dialogs.Progress;
 import de.blau.android.osm.BoundingBox;
 import de.blau.android.util.jsonreader.JsonReader;
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Search with nominatim
@@ -153,39 +156,46 @@ public class Search {
 			Uri uriBuilder = builder.appendQueryParameter("format", "jsonv2").build();
 
 			String urlString = uriBuilder.toString();
-			Log.d("Search", "urlString " + urlString);
+			Log.d("Search", "urlString: " + urlString);
+			Request request = new Request.Builder()
+					.url(urlString)
+					.build();
+			Call searchCall = Application.getHttpClient().newCall(request);
+			JsonReader reader = null;
+			ResponseBody responseBody = null;
 			try {
-				URL url = new URL(urlString);
-				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-				conn.setRequestProperty("User-Agent", Application.userAgent);
-				JsonReader reader = new JsonReader(new InputStreamReader(conn.getInputStream()));
-				ArrayList<SearchResult> result = new ArrayList<SearchResult>();
-				try {
-					try {
-						reader.beginArray();
-						while (reader.hasNext()) {
-							SearchResult searchResult = readResult(reader);
-							if (searchResult != null) {//TODO handle deprecated 
-								result.add(searchResult);
-								Log.d("Search","received: " + searchResult.toString());
-							}
+				Response searchCallResponse = searchCall.execute();
+				if (searchCallResponse.isSuccessful()) {
+					responseBody = searchCallResponse.body();
+					InputStream inputStream = responseBody.byteStream();
+					reader = new JsonReader(new InputStreamReader(inputStream));
+					ArrayList<SearchResult> result = new ArrayList<SearchResult>();
+					reader.beginArray();
+					while (reader.hasNext()) {
+						SearchResult searchResult = readResult(reader);
+						if (searchResult != null) { //TODO handle deprecated
+							result.add(searchResult);
+							Log.d("Search", "received: " + searchResult.toString());
 						}
-						reader.endArray();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
+					reader.endArray();
 					return result;
 				}
-				finally {
-				       reader.close();
-				}			
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
+			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				try {
+					if (responseBody != null) {
+						responseBody.close();
+					}
+					if (reader != null) {
+						reader.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 			return null;
 		}
