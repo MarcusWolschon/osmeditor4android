ccf9368e08d7ba3b779ae80dac1ba94ffdb37d04
simonpoole <simon@poole.ch>
2016-05-08 10:19:48 +0200

    More UI work

--- src/de/blau/android/Main.java
+++ src/de/blau/android/Main.java
@@ -2898,20 +2898,4 @@ public class Main extends BugFixedAppCompatActivity implements ServiceConnection
 			follow.show();
 		}
 	}
-	
-	@SuppressLint("NewApi")
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
-			if (hasFocus) {
-				getWindow().getDecorView().setSystemUiVisibility(
-						View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-		}
-	}
 }
--- src/de/blau/android/propertyeditor/PropertyEditor.java
+++ src/de/blau/android/propertyeditor/PropertyEditor.java
@@ -1018,21 +1018,4 @@ public class PropertyEditor extends BugFixedAppCompatActivity implements
 	public void onSupportActionModeFinished(ActionMode mode) {
 		super.onSupportActionModeFinished(mode);
 	}
-	
-	@SuppressLint("NewApi")
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-//		TODO We need to use a standalone toolbar for the actionbar to work if we do the following
-//		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
-//			if (hasFocus) {
-//				getWindow().getDecorView().setSystemUiVisibility(
-//						View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//						| View.SYSTEM_UI_FLAG_FULLSCREEN
-//						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-//		}
-	}
 }
