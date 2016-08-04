c442b835b465b7a4bab762c99a083fa290ef2b20
simonpoole <simon@poole.ch>
2016-08-04 20:26:13 +0200

    Hack to tint create dir menu icon

--- src/de/blau/android/util/ThemedFilePickerActivity.java
+++ src/de/blau/android/util/ThemedFilePickerActivity.java
@@ -2,23 +2,47 @@ package de.blau.android.util;
 
 import com.nononsenseapps.filepicker.FilePickerActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import de.blau.android.R;
 import de.blau.android.prefs.Preferences;
 
 /**
- * Simple wrapper around FilePickerActivity so that we can switch themes
+ * Simple wrapper around FilePickerActivity so that we can switch themes and a hack so that we can tint a menu icon
  * 
- * @author simon
+ * @author Simon
  *
  */
 public class ThemedFilePickerActivity extends FilePickerActivity {
-    @Override
+    private static final String DEBUG_TAG = "ThemedFilePickerActivity";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		Preferences prefs = new Preferences(this);		
 		if (prefs.lightThemeEnabled()) {
 			setTheme(R.style.FilePickerThemeLight);
 		}
     	super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+	public boolean onPrepareOptionsMenu (Menu menu) {
+    	super.onPrepareOptionsMenu(menu);
+   		MenuItem createDirItem = menu.findItem(R.id.nnf_action_createdir);
+		if (createDirItem != null) {
+			Drawable createDirIcon = DrawableCompat.wrap(createDirItem.getIcon());
+			ColorStateList tint = ContextCompat.getColorStateList(this,ThemeUtils.getResIdFromAttribute(this, R.attr.colorAccent));
+			DrawableCompat.setTintList(createDirIcon, tint);
+			createDirItem.setIcon(createDirIcon);
+		} else {
+			Log.d(DEBUG_TAG, "item not found");
+		}
+    	return true;
     }
 }
