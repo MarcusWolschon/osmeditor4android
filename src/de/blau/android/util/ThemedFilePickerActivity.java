84a88417103dcf908b8dcfdc13a47ff2800e7c35
simonpoole <simon@poole.ch>
2016-08-05 09:14:03 +0200

    Reformat

--- src/de/blau/android/util/ThemedFilePickerActivity.java
+++ src/de/blau/android/util/ThemedFilePickerActivity.java
@@ -14,35 +14,37 @@ import de.blau.android.R;
 import de.blau.android.prefs.Preferences;
 
 /**
- * Simple wrapper around FilePickerActivity so that we can switch themes and a hack so that we can tint a menu icon
+ * Simple wrapper around FilePickerActivity so that we can switch themes and a
+ * hack so that we can tint a menu icon
  * 
  * @author Simon
  *
  */
 public class ThemedFilePickerActivity extends FilePickerActivity {
-    private static final String DEBUG_TAG = "ThemedFilePickerActivity";
+	private static final String DEBUG_TAG = "ThemedFilePickerActivity";
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-		Preferences prefs = new Preferences(this);		
+	protected void onCreate(Bundle savedInstanceState) {
+		Preferences prefs = new Preferences(this);
 		if (prefs.lightThemeEnabled()) {
 			setTheme(R.style.FilePickerThemeLight);
 		}
-    	super.onCreate(savedInstanceState);
-    }
-    
-    @Override
-	public boolean onPrepareOptionsMenu (Menu menu) {
-    	super.onPrepareOptionsMenu(menu);
-   		MenuItem createDirItem = menu.findItem(R.id.nnf_action_createdir);
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		MenuItem createDirItem = menu.findItem(R.id.nnf_action_createdir);
 		if (createDirItem != null) {
 			Drawable createDirIcon = DrawableCompat.wrap(createDirItem.getIcon());
-			ColorStateList tint = ContextCompat.getColorStateList(this,ThemeUtils.getResIdFromAttribute(this, R.attr.colorAccent));
+			ColorStateList tint = ContextCompat.getColorStateList(this,
+					ThemeUtils.getResIdFromAttribute(this, R.attr.colorAccent));
 			DrawableCompat.setTintList(createDirIcon, tint);
 			createDirItem.setIcon(createDirIcon);
 		} else {
 			Log.d(DEBUG_TAG, "item not found");
 		}
-    	return true;
-    }
+		return true;
+	}
 }
