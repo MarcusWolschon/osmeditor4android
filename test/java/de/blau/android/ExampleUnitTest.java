c1354b05cfef46da507e498cc699c446285d0103
Tobias Preuss <tobias.preuss@googlemail.com>
2016-09-07 20:19:18 +0200

    Configure basic setup for unit tests.
    + As soon as the project structure will be migrated you have to
      move the "test" folder and remove the custom source set configuration.
      See discussion in issue #281.

---
+++ test/java/de/blau/android/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package de.blau.android;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Keep this simple test in your project
+// as an indicator that tests still work.
+public class ExampleUnitTest extends BaseUnitTest {
+
+    @Test
+    public void addition() throws Exception {
+        assertThat(2 + 2).isEqualTo(4);
+    }
+
+}
