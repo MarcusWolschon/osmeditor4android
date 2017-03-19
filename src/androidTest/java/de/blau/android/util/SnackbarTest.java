2ab1cd5509fdd2c9eb809112e85e22c535f4afc3
simonpoole <simon@poole.ch>
2017-03-19 22:37:50 +0100

    Add queuing mechansim dor snackbars and added some tests.

---
+++ src/androidTest/java/de/blau/android/util/SnackbarTest.java
@@ -0,0 +1,128 @@
+package de.blau.android.util;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.design.widget.Snackbar;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import de.blau.android.Main;
+
+/**
+ *
+ * @author simon
+ *
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SnackbarTest {
+	
+	Main main = null;
+	View v = null;
+	
+    @Rule
+    public ActivityTestRule<Main> mActivityRule = new ActivityTestRule<>(Main.class);
+    
+    @Before
+    public void setup() {
+    	main = mActivityRule.getActivity();
+    	v = main.findViewById(android.R.id.content);
+    }
+    
+    @Test
+	public void queue() {
+
+    	Snackbar s1 =  Snackbar.make(v, "Test1", Snackbar.LENGTH_LONG);
+    	Snackbar s2 =  Snackbar.make(v, "Test2", Snackbar.LENGTH_LONG);
+    	Snackbar s3 =  Snackbar.make(v, "Test3", Snackbar.LENGTH_LONG);
+    	Snackbar s4 =  Snackbar.make(v, "Test4", Snackbar.LENGTH_LONG);
+    	
+    	Snack.enqueue(Snack.infoQueue, s1);
+    	Snack.enqueue(Snack.infoQueue, s2);
+    	Snack.enqueue(Snack.infoQueue, s3);
+    	
+    	Assert.assertTrue(Snack.infoQueue.contains(s1));
+    	Assert.assertTrue(Snack.infoQueue.contains(s2));
+    	Assert.assertTrue(Snack.infoQueue.contains(s3));
+    	
+    	Assert.assertEquals(3, Snack.QUEUE_CAPACITY); // just to be sure that our assumptions are true
+    	Snack.enqueue(Snack.infoQueue, s4);
+    	
+       	Assert.assertFalse(Snack.infoQueue.contains(s1));
+    	Assert.assertTrue(Snack.infoQueue.contains(s2));
+    	Assert.assertTrue(Snack.infoQueue.contains(s3));
+    	Assert.assertTrue(Snack.infoQueue.contains(s4));
+	}
+
+    @Test
+	public void infoQueue() {
+		Snackbar s =  Snackbar.make(v, "Test", Snackbar.LENGTH_LONG);
+    	final CountDownLatch signal = new CountDownLatch(1);
+    	s.setCallback(new Snackbar.Callback() {
+    		@Override
+			public void onDismissed(Snackbar s, int event) {
+			}
+    		@Override
+			public void onShown (Snackbar sb) {
+    			signal.countDown();
+    		}
+    	});
+    	Snack.enqueueInfo(s);
+       	try {
+    		signal.await(10, TimeUnit.SECONDS);
+    	} catch (InterruptedException e) {
+    		Assert.fail(e.getMessage());
+    	}
+	}
+    
+    @Test
+    public void warningQueue() {
+		Snackbar s =  Snackbar.make(v, "Test", Snackbar.LENGTH_LONG);
+    	final CountDownLatch signal = new CountDownLatch(1);
+    	s.setCallback(new Snackbar.Callback() {
+    		@Override
+			public void onDismissed(Snackbar s, int event) {
+			}
+    		@Override
+			public void onShown (Snackbar sb) {
+    			signal.countDown();
+    		}
+    	});
+    	Snack.enqueueInfo(s);
+       	try {
+    		signal.await(10, TimeUnit.SECONDS);
+    	} catch (InterruptedException e) {
+    		Assert.fail(e.getMessage());
+    	}
+	}
+    
+    @Test
+    public void errorQueue() {
+		Snackbar s =  Snackbar.make(v, "Test", Snackbar.LENGTH_LONG);
+    	final CountDownLatch signal = new CountDownLatch(1);
+    	s.setCallback(new Snackbar.Callback() {
+    		@Override
+			public void onDismissed(Snackbar s, int event) {
+			}
+    		@Override
+			public void onShown (Snackbar sb) {
+    			signal.countDown();
+    		}
+    	});
+    	Snack.enqueueInfo(s);
+       	try {
+    		signal.await(10, TimeUnit.SECONDS);
+    	} catch (InterruptedException e) {
+    		Assert.fail(e.getMessage());
+    	}
+	}
+    
+}
--- src/main/java/de/blau/android/util/Snack.java
+++ src/main/java/de/blau/android/util/Snack.java
@@ -1,5 +1,7 @@
 package de.blau.android.util;
 
+import java.util.LinkedList;
+
 import android.app.Activity;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -7,14 +9,146 @@ import android.view.View;
 import de.blau.android.R;
 
 /**
- * Helper methods to display Snackbars in a consistent way
+ * Helper methods to display Snackbars in a consistent way and queuing on one of three priority queues
  * @author simon
  *
  */
 public class Snack {
 	
 	private static final int SHOW_DURATION_ACTION = 5000;
+	
+	protected static final int QUEUE_CAPACITY = 3;
+	
+	private static Object queueLock = new Object();
+	// google has declared Snackbar as final, making life difficult for everybody ....
+	protected static LinkedList<Snackbar> infoQueue = new LinkedList<Snackbar>();
+	protected static LinkedList<Snackbar> warningQueue = new LinkedList<Snackbar>();
+	protected static LinkedList<Snackbar> errorQueue = new LinkedList<Snackbar>();
 
+	static boolean isShowing(LinkedList<Snackbar> queue) {
+		Snackbar first = queue.peekFirst();
+		if (first != null) {
+			return first.isShown();
+		}
+		return false;
+	}
+	
+	/**
+	 * Enqueue a snackbar on a queue removing items if space is exhausted
+	 * 
+	 * Note: caller needs to provide synchronization
+	 * @param queue
+	 * @param s
+	 */
+	static void enqueue(LinkedList<Snackbar> queue, Snackbar s) {
+		if (queue.size() >= QUEUE_CAPACITY) {
+			Snackbar first = queue.peekFirst();
+			if (first != null) {
+				queue.removeFirst();
+				if (first.isShown()) {
+					first.dismiss(); // will try to remove itself but that is OK
+				}
+			}
+		}
+		queue.offer(s);
+	}
+	
+	/**
+	 * Show the first snackbar on the queue if any and not already being shown
+	 * 
+	 * Note: caller needs to provide synchronization
+	 * @param queue to check
+	 * @return true if a snackbar was found and shown
+	 */
+	static boolean showFirst(LinkedList<Snackbar> queue) {
+		Snackbar first = queue.peekFirst();
+		if (first != null && !first.isShown()) {
+			first.show();
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Dismiss the first entry on the queue if it is being shown
+	 * 
+	 * Note: caller needs to provide synchronization
+	 * @param queue the queue to check
+	 */
+	static void dismiss(LinkedList<Snackbar> queue) {
+		Snackbar first = queue.peekFirst();
+		if (first != null && !first.isShown()) {
+			first.dismiss();
+		}
+	}
+	
+	/**
+	 * Enqueue a snackbar on the info queue
+	 * 
+	 * @param s the snackbar to queue
+	 */
+	static void enqueueInfo(Snackbar s) {
+		synchronized (queueLock) {
+			enqueue(infoQueue, s);
+			if (!isShowing(warningQueue) && !isShowing(errorQueue)) {
+				showFirst(infoQueue);
+			}
+		}
+	}
+	
+	/**
+	 * Enqueue a snackbar on the warning queue
+	 * 
+	 * @param s the snackbar to queue
+	 */
+	static void enqueueWarning(Snackbar s) {
+		synchronized (queueLock) {
+			enqueue(warningQueue, s);
+			if (isShowing(errorQueue)) {
+				return;
+			}
+			dismiss(warningQueue);
+			showFirst(warningQueue);
+		}
+	}
+	
+	/**
+	 * Enqueue a snackbar on the error queue
+	 * 
+	 * @param s the snackbar to queue
+	 */
+	static void enqueueError(Snackbar s) {
+		synchronized (queueLock) {
+			enqueue(errorQueue, s);
+			dismiss(infoQueue);
+			dismiss(warningQueue);
+			showFirst(errorQueue);
+		}
+	}
+	
+	/**
+	 * called when a snackbar has been dismissed, removes itself from the queue 
+	 * and shows the next eligible snackbar if any. 
+	 */
+	static Snackbar.Callback callback = new Snackbar.Callback() {
+		@Override
+		public void onDismissed(Snackbar s, int event) {
+			synchronized(queueLock) {
+				if (!infoQueue.remove(s)) {
+					if (!warningQueue.remove(s)) {
+						errorQueue.remove(s);
+					}
+				}			
+				if (!showFirst(errorQueue)) {
+					if (!showFirst(warningQueue)) {
+						showFirst(infoQueue);
+					}
+				}
+			}
+		}
+	};
+	
+	
 	/**
 	 * Display a snackbar with an error message
 	 * 
@@ -36,7 +170,8 @@ public class Snack {
 	public static void barError(View v, int res) {
 		Snackbar snackbar = Snackbar.make(v, res, Snackbar.LENGTH_LONG);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_error, android.R.color.holo_red_light));
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueError(snackbar);
 	}
 	
 	/**
@@ -60,7 +195,8 @@ public class Snack {
 	public static void barError(View v, String msg) {
 		Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_LONG);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_error, android.R.color.holo_red_light));
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueError(snackbar);
 	}
 	
 	/**
@@ -117,7 +253,8 @@ public class Snack {
 	public static void barInfo(View v, int res, int duration) {
 		Snackbar snackbar = Snackbar.make(v, res, duration);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_info, android.R.color.holo_green_light));
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueInfo(snackbar);
 	}	
 	
 	/**
@@ -164,7 +301,8 @@ public class Snack {
 	public static void barInfo(View v, String msg, int duration) {
 		Snackbar snackbar = Snackbar.make(v, msg, duration);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_info, android.R.color.holo_green_light));
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueInfo(snackbar);
 	}
 	
 	/**
@@ -176,7 +314,8 @@ public class Snack {
 	public static void barInfo(View v, String msg) {
 		Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_LONG);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_info, android.R.color.holo_green_light));
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueInfo(snackbar);
 	}
 	
 	/**
@@ -207,7 +346,8 @@ public class Snack {
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_info, android.R.color.holo_green_light));
 		snackbar.setActionTextColor(ContextCompat.getColor(v.getContext(), R.color.ccc_white));
 		snackbar.setAction (actionRes, listener);
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueInfo(snackbar);
 	}
 	
 	/**
@@ -238,7 +378,8 @@ public class Snack {
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_info, android.R.color.holo_green_light));
 		snackbar.setActionTextColor(ContextCompat.getColor(v.getContext(), R.color.ccc_white));
 		snackbar.setAction(action, listener);
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueInfo(snackbar);
 	}
 
 	/**
@@ -275,7 +416,7 @@ public class Snack {
 	public static void barWarning(View v, int res, int duration) {
 		Snackbar snackbar = Snackbar.make(v, res, duration);
 		snackbar.getView().setBackgroundColor(ThemeUtils.getStyleAttribColorValue(v.getContext(), R.attr.snack_warning, android.R.color.holo_green_light));
-		// snackbar.getView().setBackgroundColor(R.color.ccc_ocher);
-		snackbar.show();
+		snackbar.setCallback(callback);
+		enqueueWarning(snackbar);
 	}	
 }
