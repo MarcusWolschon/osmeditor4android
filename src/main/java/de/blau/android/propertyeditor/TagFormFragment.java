cda01289e4acaae23f61663d9ea273662c5fe4af
simonpoole <simon@poole.ch>
2017-04-17 09:19:32 +0200

    Initial integration of OH

--- src/main/java/de/blau/android/propertyeditor/TagFormFragment.java
+++ src/main/java/de/blau/android/propertyeditor/TagFormFragment.java
@@ -61,6 +61,8 @@ import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import ch.poole.conditionalrestrictionparser.ConditionalRestrictionParser;
+import ch.poole.openinghoursfragment.OpeningHoursFragment;
+import ch.poole.openinghoursparser.OpeningHoursParser;
 import de.blau.android.App;
 import de.blau.android.HelpViewer;
 import de.blau.android.R;
@@ -790,6 +792,8 @@ public class TagFormFragment extends BaseFragment implements FormUpdate {
 						|| key.endsWith(Tags.KEY_CONDITIONAL_SUFFIX)) {
 						if (key.endsWith(Tags.KEY_CONDITIONAL_SUFFIX)) {
 							rowLayout.addView(addConditionalRestrictionDialogRow(rowLayout, preset, hint, key, value, adapter));
+						} else if (Tags.KEY_OPENING_HOURS.equals(key)) {
+							rowLayout.addView(addOpeningHoursDialogRow(rowLayout, preset, hint, key, value, adapter));
 						} else {
 							// special handling for international names
 							rowLayout.addView(addTextRow(rowLayout, preset, keyType, hint, key, value, defaultValue, adapter));
@@ -1094,6 +1098,63 @@ public class TagFormFragment extends BaseFragment implements FormUpdate {
 			    ft.commit();
 			    ConditionalRestrictionFragment conditionalRestrictionDialog = ConditionalRestrictionFragment.newInstance(key,value,templates,ohTemplates);
 			    conditionalRestrictionDialog.show(fm, "fragment_conditional_restriction");
+			}
+		});
+		return row;
+	}
+	
+	private TagFormDialogRow addOpeningHoursDialogRow(LinearLayout rowLayout, PresetItem preset, final String hint, final String key, final String value, final ArrayAdapter<?> adapter) {
+		final TagFormDialogRow row = (TagFormDialogRow)inflater.inflate(R.layout.tag_form_combo_dialog_row, rowLayout, false);
+		row.keyView.setText(hint != null?hint:key);
+		row.keyView.setTag(key);
+		row.setPreset(preset);
+
+		final ArrayList<String> templates = new ArrayList<String>();
+		Log.d(DEBUG_TAG, "adapter size " + adapter.getCount());
+		for (int i=0;i< adapter.getCount();i++) {
+			Object o = adapter.getItem(i);
+			
+			StringWithDescription swd = new StringWithDescription(o);
+			Log.d(DEBUG_TAG, "adding " + swd);
+			String v = swd.getValue();
+			if (v==null || "".equals(v)) {
+				continue;
+			}
+			if (v.equals(value)){
+				OpeningHoursParser parser = new OpeningHoursParser(new ByteArrayInputStream(v.getBytes()));
+				try {
+					row.setValue(ch.poole.openinghoursparser.Util.rulesToOpeningHoursString(parser.rules(false)));
+				} catch (Exception ex) {
+					row.setValue(v);
+				}
+			}
+			Log.d(DEBUG_TAG, "adding " + v + " to templates");
+			templates.add(v);
+		}
+		final ArrayList<String> ohTemplates = new ArrayList<String>();
+		for (StringWithDescription s:Preset.getAutocompleteValues(((PropertyEditor)getActivity()).presets,((PropertyEditor)getActivity()).getElement().getType(), Tags.KEY_OPENING_HOURS)) {
+			ohTemplates.add(s.getValue());
+		}	
+		row.valueView.setHint(R.string.tag_dialog_value_hint);
+		row.setOnClickListener(new OnClickListener() {
+			@SuppressLint("NewApi")
+			@Override
+			public void onClick(View v) {
+				final View finalView = v;
+				// finalView.setEnabled(false); // FIXME debounce 
+				FragmentManager fm = getChildFragmentManager();
+				FragmentTransaction ft = fm.beginTransaction();
+			    Fragment prev = fm.findFragmentByTag("fragment_conditional_restriction");
+			    if (prev != null) {
+			        ft.remove(prev);
+			    }
+			    ft.commit();
+			    String temp = value;
+			    if (value==null || "".equals(value)) {
+			    	temp ="Mo-Fr 09:00-12:00,13:30-18:30, Sa 09:00-17:00;PH off";
+			    }
+			    OpeningHoursFragment conditionalRestrictionDialog = OpeningHoursFragment.newInstance(key,temp,R.style.Theme_AppCompat_Light_Dialog_Alert);
+			    conditionalRestrictionDialog.show(fm, "fragment_opening_hours");
 			}
 		});
 		return row;
