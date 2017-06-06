17fe2b26ab31077677468f1a1d693422d0752ebc
simonpoole <simon@poole.ch>
2017-06-06 15:56:23 +0200

    annotations and javadoc

--- src/main/java/de/blau/android/tasks/TaskStorage.java
+++ src/main/java/de/blau/android/tasks/TaskStorage.java
@@ -8,6 +8,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import de.blau.android.R;
 import de.blau.android.osm.BoundingBox;
@@ -43,46 +44,74 @@ public class TaskStorage implements Serializable {
 		dirty = false;
 	}
 	
+	/**
+	 * Reset storage to initial values
+	 */
 	public synchronized void reset() {
 		tasks = new RTree(30,100);
 		boxes = new RTree(2,20);
 		dirty = true;
 	}
 
-	public synchronized void add(Task b) {
-		tasks.insert(b);
+	/**
+	 * Add task to storage
+	 * 
+	 * Set storage state to dirty as a side effect
+	 * @param t Task to add
+	 */
+	public synchronized void add(@NonNull Task t) {
+		tasks.insert(t);
 		dirty = true;
 	}
 	
-	public synchronized void add(BoundingBox b) {
+	/**
+	 * Add bounding box to storage
+	 * 
+	 * Set storage state to dirty as a side effect
+	 * @param b Boundingbox to add
+	 */
+	public synchronized void add(@NonNull BoundingBox b) {
 		boxes.insert(b);
 		dirty = true;
 	}
 	
-	public synchronized void delete(Task b) {
-		tasks.remove(b);
+	/**
+	 * Remove task from storage
+	 * 
+	 * Set storage state to dirty as a side effect
+	 * @param t Task to remove
+	 */
+	public synchronized void delete(@NonNull Task t) {
+		tasks.remove(t);
 		dirty = true;
 	}
 	
-	public synchronized void delete(BoundingBox b) {
+	/**
+	 * Remove bounding box from storage
+	 * 
+	 * Set storage state to dirty as a side effect
+	 * @param b BoundingBox to remove
+	 */
+	public synchronized void delete(@NonNull BoundingBox b) {
 		boxes.remove(b);
 		dirty = true;
 	}
 	
 	/**
-	 * Returns true if there is an bug with the same id in the same location 
-	 * (bug location is immutable so shoudn't be a problem)
-	 * @param b
-	 * @return
+	 * Returns true if there is a task with the same id in the same location 
+	 * (task location is immutable so shoudn't be a problem)
+	 * 
+	 * @param t Task to check for
+	 * @return true if t was found
 	 */
-	public boolean contains(Task b) {
+	public boolean contains(@NonNull Task t) {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
-		tasks.query(queryResult, b.getLon(), b.getLat());
+		tasks.query(queryResult, t.getLon(), t.getLat());
 		Log.d(DEBUG_TAG,"candidates for contain " + queryResult.size());
 		for (BoundedObject bo:queryResult) {
-			if (b instanceof Note && bo instanceof Note && b.getId() == ((Task)bo).getId()) {
+			if (t instanceof Note && bo instanceof Note && t.getId() == ((Task)bo).getId()) {
 				return true;
-			} else if (b instanceof OsmoseBug && bo instanceof OsmoseBug && b.getId() == ((Task)bo).getId()) {
+			} else if (t instanceof OsmoseBug && bo instanceof OsmoseBug && t.getId() == ((Task)bo).getId()) {
 				return true;
 			} 
 		}
@@ -90,9 +119,11 @@ public class TaskStorage implements Serializable {
 	}
 	
 	/**
-	 * Return all bugs
-	 * @return
+	 * Return all tasks in storage
+	 * 
+	 * @return a List of Tasks
 	 */
+	@NonNull 
 	public ArrayList<Task>getTasks() {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
 		tasks.query(queryResult);
@@ -104,7 +135,14 @@ public class TaskStorage implements Serializable {
 		return result;
 	}
 	
-	public ArrayList<Task>getTasks(BoundingBox box) {
+	/**
+	 * Return all tasks in a bounding box
+	 * 
+	 * @param box BoundingBox that should be searched
+	 * @return a List of Tasks
+	 */
+	@NonNull
+	public ArrayList<Task>getTasks(@NonNull BoundingBox box) {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
 		tasks.query(queryResult,box.getBounds());
 		Log.d(DEBUG_TAG,"getTasks result count " + queryResult.size());
@@ -115,18 +153,21 @@ public class TaskStorage implements Serializable {
 		return result;
 	}
 
+	/**
+	 * 
+	 * @return true if storage is empty
+	 */
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return tasks.count() == 0;
-	}
-	
+	}	
 	
 	/**
 	 * Stores the current storage data to the default storage file
-	 * @param ctx TODO
+	 * 
+	 * @param ctx Android Context
 	 * @throws IOException
 	 */
-	public synchronized void writeToFile(Context ctx) throws IOException { 
+	public synchronized void writeToFile(@NonNull Context ctx) throws IOException { 
 		if (!dirty) {
 			Log.i(DEBUG_TAG, "storage not dirty, skipping save");
 			return;
@@ -155,7 +196,7 @@ public class TaskStorage implements Serializable {
 	 * Loads the storage data from the default storage file
 	 * NOTE: lock is acquired in logic before this is called
 	 */
-	public synchronized boolean readFromFile(Context context) {
+	public synchronized boolean readFromFile(@NonNull Context context) {
 		try{
 			readingLock.lock();
 			TaskStorage newStorage = savingHelper.load(context, FILENAME, true); 
@@ -175,18 +216,28 @@ public class TaskStorage implements Serializable {
 		}
 	}
 	
+	/**
+	 * Set the state of the storage to dirty (needs to be saved)
+	 */
 	public void setDirty() {
 		dirty = true;
 	}
 	
-	public String toString() {
-		return "task r-tree: " + tasks.count() + " boxes r-tree " + boxes.count();
-	}
-
+	/**
+	 * Return a new temporary id for a task
+	 * 
+	 * @return a new temporary (negative) id
+	 */
 	public long getNextId() {
 		return newId--;
 	}
 
+	/**
+	 * Retrieve all bounding boxes from storage
+	 * 
+	 * @return list of BoundingBoxes
+	 */
+	@NonNull
 	public ArrayList<BoundingBox> getBoundingBoxes() {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
 		boxes.query(queryResult);
@@ -197,7 +248,14 @@ public class TaskStorage implements Serializable {
 		return result;
 	}
 	
-	public ArrayList<BoundingBox> getBoundingBoxes(BoundingBox box) {
+	/**
+	 * Retrieve bounding boxes that are "in" a bounding box
+	 * 
+	 * @param box BoundingBox that needs to be searched
+	 * @return list of BoundingBox intersecting box
+	 */
+	@NonNull
+	public ArrayList<BoundingBox> getBoundingBoxes(	@NonNull BoundingBox box) {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
 		boxes.query(queryResult,box.getBounds());
 		Log.d(DEBUG_TAG,"getBoundingBoxes result count " + queryResult.size());
@@ -209,8 +267,9 @@ public class TaskStorage implements Serializable {
 	}
 	
 	/**
-	 * Check for changes
-	 * @return
+	 * Check if any of the tasks has been changed
+	 * 
+	 * @return true if a changed task is found
 	 */
 	public boolean hasChanges() {
 		Collection<BoundedObject> queryResult = new ArrayList<BoundedObject>();
@@ -221,5 +280,9 @@ public class TaskStorage implements Serializable {
 			}
 		}
 		return false;
+	}
+	
+	public String toString() {
+		return "task r-tree: " + tasks.count() + " boxes r-tree " + boxes.count();
 	}
 }
