f4ccda8ecbbd6375d48e83f75f19bd750094bf77
simonpoole <simon@poole.ch>
2017-10-30 13:52:15 +0100

    Add healthcare to the list of important tags

--- src/main/java/de/blau/android/osm/Tags.java
+++ src/main/java/de/blau/android/osm/Tags.java
@@ -212,6 +212,7 @@ public class Tags {
 	public static final String KEY_HISTORIC = "historic";
 	public static final String KEY_MILITARY = "military";
 	public static final String KEY_PIPELINE = "pipeline";
+	public static final String KEY_HEALTHCARE = "healthcare";
 	public static final String KEY_XMAS_FEATURE = "xmas:feature";
 	
 	// more QA keys
@@ -226,7 +227,7 @@ public class Tags {
 		KEY_HIGHWAY,KEY_BARRIER,KEY_WATERWAY,KEY_RAILWAY,KEY_AEROWAY,KEY_AERIALWAY,KEY_POWER,
 		KEY_MAN_MADE,KEY_BUILDING,KEY_LEISURE,KEY_AMENITY,KEY_OFFICE,KEY_SHOP,KEY_CRAFT,KEY_EMERGENCY,
 		KEY_TOURISM,KEY_HISTORIC,KEY_LANDUSE,KEY_MILITARY,KEY_NATURAL,KEY_BOUNDARY,KEY_TYPE,KEY_ENTRANCE,
-		KEY_PIPELINE, KEY_XMAS_FEATURE)));
+		KEY_PIPELINE,KEY_HEALTHCARE,KEY_XMAS_FEATURE)));
 
 	/**
 	 * Tags for objects that should be re-surveyed regularly.
