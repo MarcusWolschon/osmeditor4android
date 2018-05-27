d70abb9586f219b1f3235be3e81e7dcfdd35ff65
simonpoole <simon@poole.ch>
2018-05-27 18:34:00 +0200

    Javadoc and annotations

--- src/main/java/de/blau/android/osm/RelationMember.java
+++ src/main/java/de/blau/android/osm/RelationMember.java
@@ -2,6 +2,9 @@ package de.blau.android.osm;
 
 import java.io.Serializable;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 /**
  * RelationMember stores the necessary information for a relation member, if the element field is null the element
  * itself is not present (not downloaded typically) and only the osm id, type (needed to make the id unique) and role
@@ -20,8 +23,12 @@ public class RelationMember implements Serializable {
 
     /**
      * Constructor for members that have not been downloaded
+     * 
+     * @param t type of the member OsmElement
+     * @param id the OSM id
+     * @param r the role of the element
      */
-    public RelationMember(final String t, final long id, final String r) {
+    public RelationMember(@NonNull final String t, final long id, @Nullable final String r) {
         type = t;
         ref = id;
         role = r;
@@ -29,16 +36,21 @@ public class RelationMember implements Serializable {
 
     /**
      * Constructor for members that have been downloaded
+     * 
+     * @param r the role of the element
+     * @param e the OsmElement
      */
-    public RelationMember(final String r, final OsmElement e) {
+    public RelationMember(@Nullable final String r, @NonNull final OsmElement e) {
         role = r;
         element = e;
     }
 
     /**
      * Constructor for copying, assumes that only role changes
+     * 
+     * @param rm a RelationMember instance
      */
-    public RelationMember(final RelationMember rm) {
+    public RelationMember(@NonNull final RelationMember rm) {
         if (rm.element == null) {
             type = rm.type;
             ref = rm.ref;
@@ -49,6 +61,12 @@ public class RelationMember implements Serializable {
         }
     }
 
+    /**
+     * Get the OsmElement type
+     * 
+     * @return the type (Node, way, Relation) as a String
+     */
+    @NonNull
     public String getType() {
         if (element != null) {
             return element.getName();
@@ -56,6 +74,11 @@ public class RelationMember implements Serializable {
         return type;
     }
 
+    /**
+     * Get the OSM id of the element
+     * 
+     * @return the OSM id
+     */
     public long getRef() {
         if (element != null) {
             return element.getOsmId();
@@ -63,17 +86,29 @@ public class RelationMember implements Serializable {
         return ref;
     }
 
+    /**
+     * Get the role of this relation member
+     * 
+     * @return the role or null if not set
+     */
+    @Nullable
     public String getRole() {
         return role;
     }
 
-    public void setRole(final String role) {
+    /**
+     * Set the role for the element
+     * 
+     * @param role the new role to set
+     */
+    public void setRole(@Nullable final String role) {
         this.role = role;
     }
 
     /**
      * @return the element if downloaded, null if it isn't
      */
+    @Nullable
     public OsmElement getElement() {
         return element;
     }
@@ -81,7 +116,7 @@ public class RelationMember implements Serializable {
     /**
      * set the element, used for post processing relations
      * 
-     * @param e
+     * @param e the OsmElement
      */
     public void setElement(final OsmElement e) {
         element = e;
