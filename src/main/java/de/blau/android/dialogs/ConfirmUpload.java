6c1d698c5f2ba998151179178fd8b5e5b08f18bb
simonpoole <simon@poole.ch>
2018-05-28 16:33:11 +0200

    Avoid crash on pre-Android 6 devices
    Googles documentation is not correct one time more

--- src/main/java/de/blau/android/dialogs/ConfirmUpload.java
+++ src/main/java/de/blau/android/dialogs/ConfirmUpload.java
@@ -9,13 +9,17 @@ import java.util.List;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v7.app.AlertDialog;
@@ -419,14 +423,32 @@ public class ConfirmUpload extends DialogFragment {
             if (textView != null) {
                 OsmElement element = elements[position].element;
                 if (OsmElement.STATE_DELETED != element.getState() && element.hasProblem(null, validator) != Validator.OK) {
-                    textView.setCompoundDrawableTintList(colorStateList);
+                    setTintList(textView, colorStateList);
                 } else {
-                    textView.setCompoundDrawableTintList(null);
+                    setTintList(textView, null);
                 }
             } else {
                 Log.e("ValidatorAdapterView", "position " + position + " view is null");
             }
             return v;
+        }
+
+        /**
+         * Backwards compatible way of setting the a tint list on a TextView
+         * 
+         * @param textView the TextView
+         * @param stateList the ColorStateList
+         */
+        void setTintList(@NonNull TextView textView, @Nullable ColorStateList stateList) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textView.setCompoundDrawableTintList(stateList);
+            } else {
+                for (Drawable d : textView.getCompoundDrawables()) {
+                    if (d != null) {
+                        DrawableCompat.setTintList(d, stateList);
+                    }
+                }
+            }
         }
     }
 }
