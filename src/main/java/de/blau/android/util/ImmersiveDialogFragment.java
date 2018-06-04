0ed8c6975f2af83dca01e609c3d90496f48139d6
simonpoole <simon@poole.ch>
2018-06-04 13:47:46 +0200

    Belt and suspenders

--- src/main/java/de/blau/android/util/ImmersiveDialogFragment.java
+++ src/main/java/de/blau/android/util/ImmersiveDialogFragment.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
 import android.view.WindowManager;
 
 /**
@@ -42,6 +43,11 @@ public abstract class ImmersiveDialogFragment extends DialogFragment {
         return result;
     }
 
+    /**
+     * Show a dialog in immersive mode
+     * 
+     * @param manager our FragmentManager
+     */
     @SuppressLint("NewApi")
     private void showImmersive(FragmentManager manager) {
         // It is necessary to call executePendingTransactions() on the FragmentManager
@@ -49,12 +55,17 @@ public abstract class ImmersiveDialogFragment extends DialogFragment {
         // NullPointerException since the window was not yet created.
         manager.executePendingTransactions();
 
-        // Copy flags from the activity, assuming it's fullscreen.
-        // It is important to do this after show() was called. If we would do this in onCreateDialog(),
-        // we would get a requestFeature() error.
-        getDialog().getWindow().getDecorView().setSystemUiVisibility(getActivity().getWindow().getDecorView().getSystemUiVisibility());
+        Dialog dialog = getDialog();
 
-        // Make the dialogs window focusable again
-        getDialog().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        if (dialog != null && dialog.getWindow() != null) { // seems to be an issue on some systems
+            Window dialogWindow = dialog.getWindow();
+            // Copy flags from the activity, assuming it's fullscreen.
+            // It is important to do this after show() was called. If we would do this in onCreateDialog(),
+            // we would get a requestFeature() error.
+            dialogWindow.getDecorView().setSystemUiVisibility(getActivity().getWindow().getDecorView().getSystemUiVisibility());
+
+            // Make the dialogs window focusable again
+            dialogWindow.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        }
     }
 }
