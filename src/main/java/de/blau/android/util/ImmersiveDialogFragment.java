3f18dcfd8caa5bbaf5ac8e445f76e0e9348948b0
simonpoole <simon@poole.ch>
2018-06-04 14:26:30 +0200

    Protect against getDialog returning null

--- src/main/java/de/blau/android/util/ImmersiveDialogFragment.java
+++ src/main/java/de/blau/android/util/ImmersiveDialogFragment.java
@@ -2,9 +2,11 @@ package de.blau.android.util;
 
 import android.app.Dialog;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
 import android.view.WindowManager;
 
 /**
@@ -41,18 +43,28 @@ public abstract class ImmersiveDialogFragment extends DialogFragment {
         return result;
     }
 
-    private void showImmersive(FragmentManager manager) {
+    /**
+     * Show a dialog in immersive mode
+     * 
+     * @param manager our FragmentManager
+     */
+    private void showImmersive(@NonNull FragmentManager manager) {
         // It is necessary to call executePendingTransactions() on the FragmentManager
         // before hiding the navigation bar, because otherwise getWindow() would raise a
         // NullPointerException since the window was not yet created.
         manager.executePendingTransactions();
 
-        // Copy flags from the activity, assuming it's fullscreen.
-        // It is important to do this after show() was called. If we would do this in onCreateDialog(),
-        // we would get a requestFeature() error.
-        getDialog().getWindow().getDecorView().setSystemUiVisibility(getActivity().getWindow().getDecorView().getSystemUiVisibility());
+        Dialog dialog = getDialog();
 
-        // Make the dialogs window focusable again
-        getDialog().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        if (dialog != null && dialog.getWindow() != null) { // seems to be an issue on some systems
+            Window dialogWindow = dialog.getWindow();
+            // Copy flags from the activity, assuming it's fullscreen.
+            // It is important to do this after show() was called. If we would do this in onCreateDialog(),
+            // we would get a requestFeature() error.
+            dialogWindow.getDecorView().setSystemUiVisibility(getActivity().getWindow().getDecorView().getSystemUiVisibility());
+
+            // Make the dialogs window focusable again
+            dialogWindow.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        }
     }
 }
