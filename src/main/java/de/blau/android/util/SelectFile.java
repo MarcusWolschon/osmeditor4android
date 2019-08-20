785c9dcd1f8d2ddf30615df660fa6005ac5f4ad4
simonpoole <simon@poole.ch>
2019-08-20 11:04:39 +0200

    Don't use these on older versions

--- src/main/java/de/blau/android/util/SelectFile.java
+++ src/main/java/de/blau/android/util/SelectFile.java
@@ -69,8 +69,10 @@ public final class SelectFile {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             i = new Intent(Intent.ACTION_CREATE_DOCUMENT);
             i.setType("*/*");
-            if (path != null) {
-                i.putExtra(DocumentsContract.EXTRA_INITIAL_URI, Uri.parse(path));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (path != null) {
+                    i.putExtra(DocumentsContract.EXTRA_INITIAL_URI, Uri.parse(path));
+                }
             }
         } else {
             i = new Intent(activity, ThemedFilePickerActivity.class);
@@ -110,8 +112,10 @@ public final class SelectFile {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             i.setType("*/*");
-            if (path != null) {
-                i.putExtra(DocumentsContract.EXTRA_INITIAL_URI, Uri.parse(path));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (path != null) {
+                    i.putExtra(DocumentsContract.EXTRA_INITIAL_URI, Uri.parse(path));
+                }
             }
         } else {
             i = new Intent(activity, ThemedFilePickerActivity.class);
