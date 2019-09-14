aefe38b6d68401aaacc17e62d627a05a259164fd
simonpoole <simon@poole.ch>
2019-09-15 00:58:49 +0200

    Fix legacy build

---
+++ src/legacy/java/de/blau/android/prefs/ExtendedPreferenceFragment.java
@@ -0,0 +1,46 @@
+package de.blau.android.prefs;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import ch.poole.android.numberpickerpreference.NumberPickerPreference;
+import ch.poole.android.numberpickerpreference.NumberPickerPreferenceFragment;
+import de.blau.android.prefs.MultiSelectListPreferenceDialogFragment;
+
+public abstract class ExtendedPreferenceFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public abstract void onCreatePreferences(Bundle savedInstanceState, String rootKey);
+
+    @Override
+    public void onDisplayPreferenceDialog(Preference preference) {
+        DialogFragment fragment;
+        if (preference instanceof LoginDataPreference) {
+            fragment = LoginDataPreferenceFragment.newInstance(preference);
+            fragment.setTargetFragment(this, 0);
+            fragment.show(getFragmentManager(), "android.support.v7.preference.PreferenceFragment.LOGINDATA");
+        } else if (preference instanceof MultiSelectListPreference) {
+            fragment = MultiSelectListPreferenceDialogFragment.newInstance(preference.getKey());
+            fragment.setTargetFragment(this, 0);
+            fragment.show(getFragmentManager(), "android.support.v7.preference.PreferenceFragment.MULTISELECTLIST");
+        } else if (preference instanceof NumberPickerPreference) {
+            fragment = NumberPickerPreferenceFragment.newInstance(preference.getKey());
+            fragment.setTargetFragment(this, 0);
+            fragment.show(getFragmentManager(), "android.support.v7.preference.PreferenceFragment.NUMBERPICKER");
+        } else {
+            super.onDisplayPreferenceDialog(preference);
+        }
+    }
+
+    /**
+     * Set the action bar title of the activity calling us to the PreferenceScreen title
+     */
+    protected void setTitle() {
+        AppCompatActivity activity = ((AppCompatActivity) getActivity());
+        if (activity != null) {
+            activity.getSupportActionBar().setTitle(getPreferenceScreen().getTitle());
+        }
+    }
+}
