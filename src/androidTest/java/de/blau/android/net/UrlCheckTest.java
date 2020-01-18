4493180411a5a738eb269319f74578f3aac0d077
simonpoole <simon@poole.ch>
2020-01-19 00:58:48 +0100

    Add tests for urlcheck

---
+++ src/androidTest/java/de/blau/android/net/UrlCheckTest.java
@@ -0,0 +1,111 @@
+package de.blau.android.net;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.orhanobut.mockwebserverplus.MockWebServerPlus;
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import de.blau.android.Main;
+import de.blau.android.TestUtils;
+import de.blau.android.net.UrlCheck.CheckStatus;
+import de.blau.android.prefs.Preferences;
+import de.blau.android.resources.TileLayerServer;
+import okhttp3.HttpUrl;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UrlCheckTest {
+
+    MockWebServerPlus       mockServer  = null;
+    Context                 context     = null;
+    Main                    main        = null;
+    private Instrumentation instrumentation;
+    HttpUrl                 mockBaseUrl = null;
+
+    @Rule
+    public ActivityTestRule<Main> mActivityRule = new ActivityTestRule<>(Main.class);
+
+    /**
+     * Pre-test setup
+     */
+    @Before
+    public void setup() {
+        instrumentation = InstrumentationRegistry.getInstrumentation();
+        context = instrumentation.getTargetContext();
+        main = mActivityRule.getActivity();
+        mockServer = new MockWebServerPlus();
+        mockBaseUrl = mockServer.server().url("/");
+        Preferences prefs = new Preferences(context);
+        prefs.setBackGroundLayer(TileLayerServer.LAYER_NONE); // try to avoid downloading tiles
+        prefs.setOverlayLayer(TileLayerServer.LAYER_NOOVERLAY);
+        main.getMap().setPrefs(main, prefs);
+        System.out.println("mock url " + mockBaseUrl.toString());
+        TestUtils.grantPermissons();
+        TestUtils.dismissStartUpDialogs(main);
+    }
+
+    /**
+     * Post-test teardown
+     */
+    @After
+    public void teardown() {
+        try {
+            mockServer.server().shutdown();
+        } catch (IOException ioex) {
+            System.out.println("Stopping mock webserver exception " + ioex);
+        }
+    }
+
+    /**
+     * Check http url
+     */
+    @Test
+    public void httpUrlExists() {
+        mockServer.enqueue("urlcheck1");
+        mockServer.enqueue("urlcheck2");
+        UrlCheck.Result result = UrlCheck.check(main, mockBaseUrl.host() + ":" + mockBaseUrl.port());
+        Assert.assertTrue(CheckStatus.HTTP == result.getStatus());
+        System.out.println(result.toString());
+    }
+
+    // /**
+    // * Check https url
+    // */
+    // @Test
+    // public void httpsUrlExists() {
+    // SSLContext sslContext = null;
+    // try {
+    // sslContext = SSLContext.getDefault();
+    // } catch (NoSuchAlgorithmException e) {
+    // // TODO Auto-generated catch block
+    // e.printStackTrace();
+    // }
+    // mockServer.server().useHttps(sslContext.getSocketFactory(), false);
+    // mockServer.enqueue("urlcheck1");
+    // mockServer.enqueue("urlcheck2");
+    // UrlCheck.Result result = UrlCheck.check(main, mockBaseUrl.host() + ":" + mockBaseUrl.port());
+    // System.out.println(result.toString());
+    // }
+
+    /**
+     * Check non-existent
+     */
+    @Test
+    public void noUrlExists() {
+        UrlCheck.Result result = UrlCheck.check(main, mockBaseUrl.host() + ":" + mockBaseUrl.port());
+        Assert.assertTrue(CheckStatus.UNREACHABLE == result.getStatus());
+        System.out.println(result.toString());
+    }
+}
