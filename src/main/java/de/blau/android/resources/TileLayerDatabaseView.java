aabb2bd9ee6fff41e9f8524521e54447540cbd00
simonpoole <simon@poole.ch>
2020-01-30 00:52:35 +0100

    Fix missing annotation import

--- src/main/java/de/blau/android/resources/TileLayerDatabaseView.java
+++ src/main/java/de/blau/android/resources/TileLayerDatabaseView.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface.OnDismissListener;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.CursorAdapter;
