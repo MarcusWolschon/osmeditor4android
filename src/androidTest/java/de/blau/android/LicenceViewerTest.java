7566dc67aae061082ac61bed07953a79de791cee
simonpoole <simon@poole.ch>
2020-02-14 00:43:19 +0100

    Add minimal tests for LicenseViewer and DebugInformation page

---
+++ src/androidTest/java/de/blau/android/LicenceViewerTest.java
@@ -0,0 +1,77 @@
+package de.blau.android;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.orhanobut.mockwebserverplus.MockWebServerPlus;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import de.blau.android.prefs.AdvancedPrefDatabase;
+import de.blau.android.prefs.Preferences;
+import de.blau.android.resources.TileLayerServer;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LicenceViewerTest {
+
+    MockWebServerPlus    mockServer      = null;
+    Context              context         = null;
+    ActivityMonitor      monitor         = null;
+    AdvancedPrefDatabase prefDB          = null;
+    Instrumentation      instrumentation = null;
+    UiDevice             device          = null;
+    Main                 main            = null;
+
+    @Rule
+    public ActivityTestRule<Main> mActivityRule = new ActivityTestRule<>(Main.class);
+
+    /**
+     * Pre-test setup
+     */
+    @Before
+    public void setup() {
+        instrumentation = InstrumentationRegistry.getInstrumentation();
+        device = UiDevice.getInstance(instrumentation);
+        context = instrumentation.getTargetContext();
+        monitor = instrumentation.addMonitor(LicenseViewer.class.getName(), null, false);
+        main = (Main) mActivityRule.getActivity();
+        Preferences prefs = new Preferences(context);
+        prefs.setBackGroundLayer(TileLayerServer.LAYER_NONE); // try to avoid downloading tiles
+        prefs.setOverlayLayer(TileLayerServer.LAYER_NOOVERLAY);
+        main.getMap().setPrefs(main, prefs);
+
+        TestUtils.grantPermissons();
+        TestUtils.dismissStartUpDialogs(main);
+    }
+
+    /**
+     * Post-test teardown
+     */
+    @After
+    public void teardown() {
+        instrumentation.removeMonitor(monitor);
+    }
+
+    /**
+     * Start up the HelpViewer
+     */
+    @Test
+    public void startHelp() {
+        TestUtils.clickOverflowButton();
+        TestUtils.clickText(device, false, "Authors and licenses", true);
+        Activity licenseViewer = instrumentation.waitForMonitorWithTimeout(monitor, 30000);
+        Assert.assertTrue(licenseViewer instanceof LicenseViewer);
+    }
+}
