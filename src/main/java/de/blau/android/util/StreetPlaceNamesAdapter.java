b0a947b379f03efb7754c375395c37d836331b40
simonpoole <simon@poole.ch>
2020-02-28 13:05:53 +0100

    Removed GPL licence statement
    For historical reasons this file was, conflicting with the rest of the
    project, licensed on GPL terms. Marcus Wolschon the original author and
    Simon Poole have both given permission to change the licence to Apache
    2.0 as for the whole project.

--- src/main/java/de/blau/android/util/StreetPlaceNamesAdapter.java
+++ src/main/java/de/blau/android/util/StreetPlaceNamesAdapter.java
@@ -1,25 +1,3 @@
-/**
- * TagKeyAutocompletionAdapter.java created: 12.06.2010 10:43:37 (c) 2010 by <a href="http://Wolschon.biz">Wolschon
- * Softwaredesign und Beratung</a> This file is part of OSMEditor by Marcus Wolschon
- * <a href="mailto:Marcus@Wolscon.biz">Marcus@Wolscon.biz</a>. You can purchase support for a sensible hourly rate or a
- * commercial license of this file (unless modified by others) by contacting him directly.
- *
- * OSMEditor is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * OSMEditor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with OSMEditor. If not, see
- * <http://www.gnu.org/licenses/>.
- *
- ***********************************
- * Editing this file: -For consistent code-quality this file should be checked with the checkstyle-ruleset enclosed in
- * this project. -After the design of this file has settled it should get it's own JUnit-Test that shall be executed
- * regularly. It is best to write the test-case BEFORE writing this class and to run it on every build as a
- * regression-test.
- */
 package de.blau.android.util;
 
 // other imports
@@ -41,11 +19,12 @@ import de.blau.android.presets.ValueWithCount;
 import de.blau.android.propertyeditor.PropertyEditor;
 
 /**
- * Project: OSMEditor<br/>
- * TagKeyAutocompletionAdapter.java<br/>
- * created: 12.06.2010 10:43:37 <br/>
- * <br/>
- * <br/>
+ * Project: OSMEditor
+ * 
+ * TagKeyAutocompletionAdapter.java
+ * 
+ * created: 12.06.2010 10:43:37
+ * 
  * <b>Adapter for the {@link AutoCompleteTextView} in the {@link PropertyEditor} that is for the VALUE for the key
  * "addr:street" .</b>
  * 
