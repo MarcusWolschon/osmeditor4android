ba52cc036c5e51936c6c54f639e24feac01acfcf
simonpoole <simon@poole.ch>
2020-03-10 23:00:53 +0100

    Add some more logging mocked methods

--- src/test/java/android/util/Log.java
+++ src/test/java/android/util/Log.java
@@ -5,6 +5,12 @@ package android.util;
  *
  */
 public class Log {
+
+    public final static int DEBUG = 3;
+    public final static int INFO  = 4;
+    public final static int WARN  = 5;
+    public final static int ERROR = 6;
+
     /**
      * Debug log message
      * 
@@ -14,6 +20,19 @@ public class Log {
      */
     public static int d(String tag, String msg) {
         System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    /**
+     * Debug log message
+     * 
+     * @param tag log tag
+     * @param msg message
+     * @param e Exception that caused the error
+     * @return not documented
+     */
+    public static int d(String tag, String msg, Exception e) {
+        System.out.println("DEBUG: " + tag + ": " + msg + " " + e.getMessage());
         return 0;
     }
 
@@ -42,6 +61,19 @@ public class Log {
     }
 
     /**
+     * Warning log message
+     * 
+     * @param tag log tag
+     * @param msg message
+     * @param e Exception that caused the error
+     * @return not documented
+     */
+    public static int w(String tag, String msg, Exception e) {
+        System.out.println("WARN: " + tag + ": " + msg + " " + e.getMessage());
+        return 0;
+    }
+
+    /**
      * Error log message
      * 
      * @param tag log tag
@@ -51,5 +83,29 @@ public class Log {
     public static int e(String tag, String msg) {
         System.out.println("ERROR: " + tag + ": " + msg);
         return 0;
+    }
+
+    /**
+     * Error log message
+     * 
+     * @param tag log tag
+     * @param msg message
+     * @param e Exception that caused the error
+     * @return not documented
+     */
+    public static int e(String tag, String msg, Exception e) {
+        System.out.println("ERROR: " + tag + ": " + msg + " " + e.getMessage());
+        return 0;
+    }
+
+    /**
+     * Check if the the tag can be logged
+     * 
+     * @param tag tag
+     * @param logLevel log level
+     * @return always true
+     */
+    public static boolean isLoggable(String tag, int logLevel) {
+        return true;
     }
 }
