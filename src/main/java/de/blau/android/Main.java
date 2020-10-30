55786246eea9005b787333ba94929fac47faa004
simonpoole <simon@poole.ch>
2020-10-30 22:18:20 +0100

    Ignore KEYCODE_MOVE_HOME

--- src/main/java/de/blau/android/Main.java
+++ src/main/java/de/blau/android/Main.java
@@ -3839,6 +3839,7 @@ public class Main extends FullScreenAppCompatActivity
                         logic.zoom(Logic.ZOOM_OUT);
                         updateZoomControls();
                         return true;
+                    case KeyEvent.KEYCODE_MOVE_HOME:
                     case KeyEvent.KEYCODE_CTRL_LEFT:
                     case KeyEvent.KEYCODE_CTRL_RIGHT:
                     case KeyEvent.KEYCODE_SHIFT_LEFT:
