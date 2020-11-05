65e7b9d59daa8f5f074e3b4559e3aac97c7f50c7
simonpoole <simon@poole.ch>
2020-11-05 12:43:16 +0100

    Try to guarantee that the dialog buttons are shown

--- src/main/java/de/blau/android/tasks/TaskFragment.java
+++ src/main/java/de/blau/android/tasks/TaskFragment.java
@@ -14,6 +14,7 @@ import android.text.util.Linkify;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -423,6 +424,8 @@ public class TaskFragment extends ImmersiveDialogFragment {
                         }
                     });
                 });
+        // this should keep the buttons visible 
+        d.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         return d;
     }
 
