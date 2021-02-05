44a8cf63222835e3fc131ff1ac23dd4bab581099
simonpoole <simon@poole.ch>
2021-02-05 01:03:10 +0100

    Remove unused import

--- src/main/java/de/blau/android/easyedit/ExtendSelectionActionModeCallback.java
+++ src/main/java/de/blau/android/easyedit/ExtendSelectionActionModeCallback.java
@@ -1,9 +1,7 @@
 package de.blau.android.easyedit;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import android.annotation.SuppressLint;
 import android.util.Log;
