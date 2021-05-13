700898248457b4263a6f7e07494fc34b8f90b4d8
simonpoole <simon@poole.ch>
2021-05-13 13:48:06 +0200

    Fix bogus import

--- src/main/java/de/blau/android/propertyeditor/EditorUpdate.java
+++ src/main/java/de/blau/android/propertyeditor/EditorUpdate.java
@@ -4,8 +4,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Nonnull;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import de.blau.android.names.Names;
