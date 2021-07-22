eadff78d0774e3e3f5abf2d3d92bb512c83359b1
simonpoole <simon@poole.ch>
2021-07-22 17:14:52 +0200

    Always return and don't use break in onOptionsItemSelected

--- src/main/java/de/blau/android/Main.java
+++ src/main/java/de/blau/android/Main.java
@@ -1943,10 +1943,10 @@ public class Main extends FullScreenAppCompatActivity
                 item.setChecked(true);
                 showSimpleActionsButton();
             }
-            break;
+            return true;
         case R.id.menu_share:
             Util.sharePosition(this, map.getViewBox().getCenter(), map.getZoomLevel());
-            break;
+            return true;
 
         case R.id.menu_help:
             HelpViewer.start(this, R.string.help_main);
@@ -2131,7 +2131,7 @@ public class Main extends FullScreenAppCompatActivity
             }
             startStopAutoDownload();
             map.setPrefs(this, prefs);
-            break;
+            return true;
         case R.id.menu_enable_pan_and_zoom_auto_download:
             Log.d(DEBUG_TAG, "pan and zoom auto download menu");
             if (prefs.getPanAndZoomAutoDownload()) {
@@ -2146,7 +2146,7 @@ public class Main extends FullScreenAppCompatActivity
             }
             startStopAutoDownload();
             map.setPrefs(this, prefs);
-            break;
+            return true;
         case R.id.menu_transfer_download_current:
             onMenuDownloadCurrent(true);
             return true;
@@ -2261,7 +2261,7 @@ public class Main extends FullScreenAppCompatActivity
         case R.id.menu_transfer_download_msf:
             descheduleAutoLock();
             DownloadActivity.start(this, Urls.MSF_SERVER);
-            break;
+            return true;
         case R.id.menu_transfer_bugs_download_current:
             downLoadBugs(map.getViewBox().copy());
             return true;
