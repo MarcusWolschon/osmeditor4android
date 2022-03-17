9e810d3cdd8a4777eb77b7fb0e3b8d7a61a8ca4e
simonpoole <simon@poole.ch>
2022-03-17 17:13:27 +0100

    Use josm scheme instead of localhost for alert intents
    Using htto://127.0.0.1/ for JOSM RC causes issues on Android 12. To fix
    we use josm: instead that we already supported on input.

--- src/main/java/de/blau/android/RemoteControlUrlActivity.java
+++ src/main/java/de/blau/android/RemoteControlUrlActivity.java
@@ -11,6 +11,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.util.Log;
 import androidx.annotation.Nullable;
+import de.blau.android.contract.Schemes;
 import de.blau.android.layer.LayerType;
 import de.blau.android.osm.BoundingBox;
 import de.blau.android.resources.TileLayerDatabase;
@@ -24,6 +25,23 @@ public class RemoteControlUrlActivity extends UrlActivity {
 
     private static final String DEBUG_TAG = "RemoteControlUrlAct...";
     public static final String  RCDATA    = "de.blau.android.RemoteControlActivity";
+
+    private static final String IMAGERY_COMMAND             = "imagery";
+    public static final String  LOAD_AND_ZOOM_COMMAND       = "load_and_zoom";
+    private static final String ZOOM_COMMAND                = "zoom";
+    private static final String TILE_SIZE_PARAMETER         = "tileSize";
+    private static final String MAX_ZOOM_PARAMETER          = "max_zoom";
+    private static final String MIN_ZOOM_PARAMETER          = "min_zoom";
+    private static final String TYPE_PARAMETER              = "type";
+    private static final String TITLE_PARAMETER             = "title";
+    private static final String URL_PARAMETER               = "url";
+    public static final String  SELECT_PARAMETER            = "select";
+    private static final String CHANGESET_SOURCE_PARAMETER  = "changeset_source";
+    private static final String CHANGESET_COMMENT_PARAMETER = "changeset_comment";
+    public static final String  TOP_PARAMETER               = "top";
+    public static final String  BOTTOM_PARAMETER            = "bottom";
+    public static final String  RIGHT_PARAMETER             = "right";
+    public static final String  LEFT_PARAMETER              = "left";
 
     public static class RemoteControlUrlData implements Serializable {
         private static final long serialVersionUID = 2L;
@@ -120,7 +138,7 @@ public class RemoteControlUrlActivity extends UrlActivity {
         try {
             // extract command
             String command = data.getPath();
-            if ("josm".equals(data.getScheme())) { // extract command from scheme specific part
+            if (Schemes.JOSM.equals(data.getScheme())) { // extract command from scheme specific part
                 command = data.getSchemeSpecificPart();
                 if (command != null) {
                     int q = command.indexOf('?');
@@ -140,14 +158,14 @@ public class RemoteControlUrlActivity extends UrlActivity {
             Log.d(DEBUG_TAG, "Command: " + command);
             Log.d(DEBUG_TAG, "Query: " + data.getQuery());
             switch (command) {
-            case "zoom":
-            case "load_and_zoom":
+            case ZOOM_COMMAND:
+            case LOAD_AND_ZOOM_COMMAND:
                 RemoteControlUrlData rcData = new RemoteControlUrlData();
-                rcData.setLoad("load_and_zoom".equals(command));
-                String leftParam = data.getQueryParameter("left");
-                String rightParam = data.getQueryParameter("right");
-                String bottomParam = data.getQueryParameter("bottom");
-                String topParam = data.getQueryParameter("top");
+                rcData.setLoad(LOAD_AND_ZOOM_COMMAND.equals(command));
+                String leftParam = data.getQueryParameter(LEFT_PARAMETER);
+                String rightParam = data.getQueryParameter(RIGHT_PARAMETER);
+                String bottomParam = data.getQueryParameter(BOTTOM_PARAMETER);
+                String topParam = data.getQueryParameter(TOP_PARAMETER);
 
                 if (leftParam != null && rightParam != null && bottomParam != null && topParam != null) {
                     try {
@@ -163,19 +181,19 @@ public class RemoteControlUrlActivity extends UrlActivity {
                     }
                 }
 
-                rcData.setChangesetComment(data.getQueryParameter("changeset_comment"));
-                rcData.setChangesetSource(data.getQueryParameter("changeset_source"));
+                rcData.setChangesetComment(data.getQueryParameter(CHANGESET_COMMENT_PARAMETER));
+                rcData.setChangesetSource(data.getQueryParameter(CHANGESET_SOURCE_PARAMETER));
 
-                String select = data.getQueryParameter("select");
+                String select = data.getQueryParameter(SELECT_PARAMETER);
                 if (rcData.load() && select != null) {
                     rcData.setSelect(select);
                 }
                 intent.putExtra(RCDATA, rcData);
                 return true;
-            case "imagery":
-                String url = data.getQueryParameter("url");
+            case IMAGERY_COMMAND:
+                String url = data.getQueryParameter(URL_PARAMETER);
                 if (url != null) {
-                    String title = data.getQueryParameter("title");
+                    String title = data.getQueryParameter(TITLE_PARAMETER);
                     if (title == null) {
                         try {
                             title = new URL(url).getHost() + " " + DateFormatter.getFormattedString("YYYY-MM-DD HH:mm");
@@ -190,27 +208,27 @@ public class RemoteControlUrlActivity extends UrlActivity {
                     if (ids.contains(id)) {
                         existing = TileLayerSource.get(this, id, false);
                     }
-                    String type = data.getQueryParameter("type");
+                    String type = data.getQueryParameter(TYPE_PARAMETER);
                     if (!TileLayerSource.TYPE_TMS.equals(type) && !TileLayerSource.TYPE_WMS.equals(type)) {
                         Log.e(DEBUG_TAG, "Unsupported type " + type);
                         return false;
                     }
-                    // unused and undocumented String cookies = data.getQueryParameter("cookies");
+                    // unused and undocumented String cookies = data.getQueryParameter("cookies"); NOSONAR
                     int minZoom = TileLayerSource.DEFAULT_MIN_ZOOM;
                     try { // NOSONAR
-                        minZoom = Integer.parseInt(data.getQueryParameter("min_zoom"));
+                        minZoom = Integer.parseInt(data.getQueryParameter(MIN_ZOOM_PARAMETER));
                     } catch (Exception e) {
                         // ignore
                     }
                     int maxZoom = TileLayerSource.DEFAULT_MAX_ZOOM;
                     try { // NOSONAR
-                        maxZoom = Integer.parseInt(data.getQueryParameter("max_zoom"));
+                        maxZoom = Integer.parseInt(data.getQueryParameter(MAX_ZOOM_PARAMETER));
                     } catch (Exception e) {
                         // ignore
                     }
                     int tileSize = TileLayerSource.DEFAULT_TILE_SIZE;
                     try { // NOSONAR
-                        tileSize = Integer.parseInt(data.getQueryParameter("tileSize"));
+                        tileSize = Integer.parseInt(data.getQueryParameter(TILE_SIZE_PARAMETER));
                     } catch (Exception e) {
                         // ignore
                     }
--- src/main/java/de/blau/android/contract/Schemes.java
+++ src/main/java/de/blau/android/contract/Schemes.java
@@ -3,6 +3,8 @@ package de.blau.android.contract;
 public final class Schemes {
     public static final String FILE    = "file";
     public static final String CONTENT = "content";
+    public static final String JOSM    = "josm";
+    public static final String GEO     = "geo";
 
     /**
      * Private default constructor
