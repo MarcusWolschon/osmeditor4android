9a82c063d1f70c80114ccc95a00e698749e3c660
simonpoole <simon@poole.ch>
2022-03-25 09:37:50 +0100

    Add tests for auto applying preset when preset filter is active

--- src/androidTest/java/de/blau/android/filter/PresetFilterTest.java
+++ src/androidTest/java/de/blau/android/filter/PresetFilterTest.java
@@ -1,6 +1,7 @@
 package de.blau.android.filter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
@@ -23,22 +24,25 @@ import androidx.test.uiautomator.UiDevice;
 import de.blau.android.App;
 import de.blau.android.LayerUtils;
 import de.blau.android.Main;
+import de.blau.android.Map;
 import de.blau.android.R;
 import de.blau.android.TestUtils;
+import de.blau.android.osm.Node;
+import de.blau.android.osm.Tags;
 import de.blau.android.prefs.AdvancedPrefDatabase;
 import de.blau.android.prefs.Preferences;
+import de.blau.android.propertyeditor.PropertyEditor;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class PresetFilterTest {
 
-    MockWebServerPlus    mockServer      = null;
-    Context              context         = null;
-    ActivityMonitor      monitor         = null;
-    AdvancedPrefDatabase prefDB          = null;
-    Instrumentation      instrumentation = null;
-    UiDevice             device          = null;
-    Main                 main            = null;
+    private Context         context         = null;
+    private ActivityMonitor monitor         = null;
+    private Instrumentation instrumentation = null;
+    private UiDevice        device          = null;
+    private Main            main            = null;
+    private Map             map             = null;
 
     @Rule
     public ActivityTestRule<Main> mActivityRule = new ActivityTestRule<>(Main.class);
@@ -55,7 +59,8 @@ public class PresetFilterTest {
         main = mActivityRule.getActivity();
         Preferences prefs = new Preferences(context);
         LayerUtils.removeImageryLayers(context);
-        main.getMap().setPrefs(main, prefs);
+        map = main.getMap();
+        map.setPrefs(main, prefs);
 
         TestUtils.grantPermissons(device);
         TestUtils.dismissStartUpDialogs(device, main);
@@ -75,7 +80,7 @@ public class PresetFilterTest {
      * Set preset filter to Church and select one
      */
     @Test
-    public void presetFilter() {
+    public void presetFilterSelect() {
         TestUtils.unlock(device);
         TestUtils.clickOverflowButton(device);
         TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
@@ -89,6 +94,92 @@ public class PresetFilterTest {
         TestUtils.clickAtCoordinates(device, main.getMap(), 8.38819D, 47.38961D);
         assertTrue(TestUtils.findText(device, false, main.getString(R.string.actionmode_wayselect), 5000));
         assertEquals(206010144L, App.getLogic().getSelectedWay().getOsmId());
+        TestUtils.clickUp(device);
+        TestUtils.clickOverflowButton(device);
+        TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
+    }
+
+    /**
+     * Set preset filter to Church and create one
+     */
+    @Test
+    public void presetFilterCreate() {
+        TestUtils.zoomToLevel(device, main, 21);
+        TestUtils.unlock(device);
+        TestUtils.clickOverflowButton(device);
+        TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
+        TestUtils.clickResource(device, false, device.getCurrentPackageName() + ":id/tagFilterButton", true);
+        Activity presetFilterActivity = instrumentation.waitForMonitorWithTimeout(monitor, 30000);
+        instrumentation.removeMonitor(monitor);
+        assertTrue(presetFilterActivity instanceof PresetFilterActivity);
+        assertTrue(TestUtils.clickText(device, false, "Facilities", true));
+        assertTrue(TestUtils.clickText(device, false, "Place of Worship", true, true));
+        assertTrue(TestUtils.clickText(device, false, "Church", true));
+        TestUtils.sleep(2000);
+
+        TestUtils.clickSimpleButton(device);
+        assertTrue(TestUtils.clickText(device, false, context.getString(R.string.menu_add_node_tags), true, false));
+        assertTrue(TestUtils.findText(device, false, context.getString(R.string.simple_add_node)));
+        TestUtils.clickAtCoordinates(device, map, 8.3893454, 47.3901898, true);
+
+        monitor = instrumentation.addMonitor(PropertyEditor.class.getName(), null, false);
+
+        Activity propertyEditor = instrumentation.waitForMonitorWithTimeout(monitor, 30000);
+        assertTrue(propertyEditor instanceof PropertyEditor);
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.menu_tags), 5000));
+        assertTrue(TestUtils.findText(device, false, "Church", 5000));
+
+        TestUtils.clickHome(device, true);
+        assertTrue(TestUtils.findText(device, false, context.getString(R.string.actionmode_nodeselect)));
+        device.waitForIdle();
+        Node n = App.getLogic().getSelectedNode();
+        assertNotNull(n);
+        assertTrue(n.hasTag(Tags.KEY_AMENITY, "place_of_worship"));
+
+        TestUtils.clickUp(device);
+        TestUtils.clickOverflowButton(device);
+        TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
+    }
+
+    /**
+     * Set preset filter to place or Worship group and create a church
+     */
+    @Test
+    public void presetFilterCreateGroup() {
+        TestUtils.zoomToLevel(device, main, 21);
+        TestUtils.unlock(device);
+        TestUtils.clickOverflowButton(device);
+        TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
+        TestUtils.clickResource(device, false, device.getCurrentPackageName() + ":id/tagFilterButton", true);
+        Activity presetFilterActivity = instrumentation.waitForMonitorWithTimeout(monitor, 30000);
+        instrumentation.removeMonitor(monitor);
+        assertTrue(presetFilterActivity instanceof PresetFilterActivity);
+        assertTrue(TestUtils.clickText(device, false, "Facilities", true));
+        assertTrue(TestUtils.longClickText(device, "Place of Worship"));
+        TestUtils.sleep(2000);
+
+        TestUtils.clickSimpleButton(device);
+        assertTrue(TestUtils.clickText(device, false, context.getString(R.string.menu_add_node_tags), true, false));
+        assertTrue(TestUtils.findText(device, false, context.getString(R.string.simple_add_node)));
+        TestUtils.clickAtCoordinates(device, map, 8.3893454, 47.3901898, true);
+
+        monitor = instrumentation.addMonitor(PropertyEditor.class.getName(), null, false);
+
+        Activity propertyEditor = instrumentation.waitForMonitorWithTimeout(monitor, 30000);
+        assertTrue(propertyEditor instanceof PropertyEditor);
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.tag_menu_preset), 5000));
+        assertTrue(TestUtils.findText(device, false, "Mosque", 5000));
+        assertTrue(TestUtils.clickText(device, false, "Church", false, false));
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.menu_tags), 5000));
+        assertTrue(TestUtils.findText(device, false, "Church", 5000));
+
+        TestUtils.clickHome(device, true);
+        assertTrue(TestUtils.findText(device, false, context.getString(R.string.actionmode_nodeselect)));
+        device.waitForIdle();
+        Node n = App.getLogic().getSelectedNode();
+        assertNotNull(n);
+        assertTrue(n.hasTag(Tags.KEY_AMENITY, "place_of_worship"));
+
         TestUtils.clickUp(device);
         TestUtils.clickOverflowButton(device);
         TestUtils.clickText(device, false, main.getString(R.string.menu_enable_presetfilter), true, false);
