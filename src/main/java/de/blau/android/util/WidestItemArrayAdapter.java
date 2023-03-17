7c5a2c15d8f9eee415dce7046b1defdc32fc2b0b
simonpoole <simon@poole.ch>
2023-03-17 14:37:44 +0100

    Wrap search results in a HorizintalScrollView
    Fixes https://github.com/MarcusWolschon/osmeditor4android/issues/1981

---
+++ src/main/java/de/blau/android/util/WidestItemArrayAdapter.java
@@ -0,0 +1,58 @@
+package de.blau.android.util;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+
+/**
+ * ArrayAdapter that sets the width of all views to that of the widest item.
+ * 
+ * Pre-computes the widest view in the adapter, use this when you need to wrap_content on a ListView, please be careful
+ * and don't use it on an adapter that is extremely numerous in items or it will take a long time.
+ * 
+ * Yes, this is a hack.
+ * 
+ * Based on https://stackoverflow.com/a/13959716
+ */
+public class WidestItemArrayAdapter<T> extends ArrayAdapter<T> {
+
+    int width = -1;
+
+    /**
+     * Construct a new adapter
+     * 
+     * @param context Android Context
+     * @param resource resource id of the item layout
+     * @param objects the List of objects
+     */
+    public WidestItemArrayAdapter(@NonNull Context context, int resource, @NonNull List<T> objects) {
+        super(context, resource, objects);
+        View view = null;
+        FrameLayout fakeParent = new FrameLayout(context);
+        int maxWidth = 0;
+        int count = getCount();
+        for (int i = 0; i < count; i++) {
+            view = getView(i, view, fakeParent);
+            view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            int tempWidth = view.getMeasuredWidth();
+            if (tempWidth > maxWidth) {
+                maxWidth = tempWidth;
+            }
+        }
+        width = maxWidth;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
+        if (width > 0) {
+            v.setMinimumWidth(width);
+        }
+        return v;
+    }
+}
