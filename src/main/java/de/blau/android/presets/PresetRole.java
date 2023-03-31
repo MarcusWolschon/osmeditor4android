a4a9c49f6ff73e710fa557749c6eff3de761ebae
simonpoole <simon@poole.ch>
2023-03-31 18:20:14 +0200

    Fix copy constructor

--- src/main/java/de/blau/android/presets/PresetRole.java
+++ src/main/java/de/blau/android/presets/PresetRole.java
@@ -103,6 +103,7 @@ public class PresetRole implements Comparable<PresetRole> {
         this.appliesToNode = field.appliesToNode;
         this.appliesToRelation = field.appliesToRelation;
         this.appliesToClosedWay = field.appliesToClosedWay;
+        this.appliesToArea = field.appliesToArea;
     }
 
     /**
