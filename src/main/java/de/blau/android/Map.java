MarcusWolschon/osmeditor4android

commit 3b70cf8542a1ae9d702829eb0f342af60f5cecc2

Move imagery alignment to layer dialog
This moves starting imagery alignment to the layer modal and makes it
available for overlays too.

src/main/java/de/blau/android/Map.java
@@ -258,9 +258,7 @@ public class Map extends View implements IMapView {
                                     ((de.blau.android.layer.gpx.MapOverlay) layer).setTrack(getTracker().getTrack());
                                 }
                             } else if (!((de.blau.android.layer.gpx.MapOverlay) layer).fromFile(ctx, Uri.parse(contentId))) {
-                                db.deleteLayer(LayerType.GPX, contentId);
-                                Log.w(DEBUG_TAG, "Deleted GPX layer for " + contentId);
-                                continue; // skip
+                                layer = null; // this will delete the layer
                             }
                             break;
                         case TASKS:
@@ -270,8 +268,7 @@ public class Map extends View implements IMapView {
                             layer = new de.blau.android.layer.geojson.MapOverlay(this);
                             if (!((de.blau.android.layer.geojson.MapOverlay) layer).loadGeoJsonFile(ctx, Uri.parse(contentId), true)) {
                                 // other error, has already been toasted
-                                db.deleteLayer(LayerType.GEOJSON, contentId);
-                                continue;
+                                layer = null; // this will delete the layer
                             }
                             break;
                         case MAPILLARY:
@@ -291,6 +288,10 @@ public class Map extends View implements IMapView {
                     if (LayerType.IMAGERY.equals(type) || LayerType.OVERLAYIMAGERY.equals(type)) {
                         ImageryOffsetUtils.applyImageryOffsets(ctx, prefs, ((MapTilesLayer<Bitmap>) layer).getTileLayerConfiguration(), getViewBox());
                     }
+                } else {
+                    // remove layers from DB for which the content is missing
+                    db.deleteLayer(type, contentId);
+                    Log.w(DEBUG_TAG, "Deleted " + type + " layer for " + contentId);
                 }
             }
         }
@@ -408,6 +409,29 @@ public class Map extends View implements IMapView {
             }
         }
         return null;
+    }
+
+    /**
+     * Check if layer is visible
+     * 
+     * @param layer the layer to check
+     * @return true if the layer is visible
+     */
+    public boolean isVisible(@NonNull MapViewLayer layer) {
+        List<MapViewLayer> layers = new ArrayList<>();
+        synchronized (mLayers) {
+            layers.addAll(mLayers);
+        }
+        Collections.reverse(layers);
+        for (MapViewLayer l : layers) {
+            if (layer.equals(l)) {
+                return layer.isVisible();
+            } else if (l.getType() == LayerType.IMAGERY && l.isVisible()) {
+                return false;
+            }
+        }
+        Log.e(DEBUG_TAG, "inconsistent layer config, didn't find layer " + layer.getContentId());
+        return false;
     }
 
     /**
@@ -562,10 +586,6 @@ public class Map extends View implements IMapView {
         clipBox.set(myViewBox);
         clipBox.scale(1.1);
 
-        final Logic logic = App.getLogic();
-        final Mode tmpDrawingEditMode = logic.getMode();
-        tmpLocked = logic.isLocked();
-
         // Draw our Overlays.
         canvas.getClipBounds(canvasBounds);
 
@@ -592,7 +612,8 @@ public class Map extends View implements IMapView {
             }
         }
 
-        final boolean backgroundAlignMode = tmpDrawingEditMode == Mode.MODE_ALIGN_BACKGROUND;
+        final Logic logic = App.getLogic();
+        final boolean backgroundAlignMode = logic.getMode() == Mode.MODE_ALIGN_BACKGROUND;
         if (zoomLevel > STORAGE_BOX_LIMIT && !backgroundAlignMode) {
             // shallow copy to avoid modification issues
             boundingBoxes.clear();
@@ -600,7 +621,7 @@ public class Map extends View implements IMapView {
             paintStorageBox(canvas, boundingBoxes);
         }
         paintGpsPos(canvas);
-        if (App.getLogic().isInEditZoomRange()) {
+        if (showCrosshairs && logic.isInEditZoomRange()) {
             paintCrosshairs(canvas);
         }
 
@@ -689,15 +710,12 @@ public class Map extends View implements IMapView {
      * @param canvas the Canvas to draw on
      */
     private void paintCrosshairs(@NonNull Canvas canvas) {
-        //
-        if (showCrosshairs) {
-            float x = GeoMath.lonE7ToX(getWidth(), getViewBox(), crosshairsLon);
-            float y = GeoMath.latE7ToY(getHeight(), getWidth(), getViewBox(), crosshairsLat);
-            Paint paint = DataStyle.getInternal(DataStyle.CROSSHAIRS_HALO).getPaint();
-            drawCrosshairs(canvas, x, y, paint);
-            paint = DataStyle.getInternal(DataStyle.CROSSHAIRS).getPaint();
-            drawCrosshairs(canvas, x, y, paint);
-        }
+        float x = GeoMath.lonE7ToX(getWidth(), getViewBox(), crosshairsLon);
+        float y = GeoMath.latE7ToY(getHeight(), getWidth(), getViewBox(), crosshairsLat);
+        Paint paint = DataStyle.getInternal(DataStyle.CROSSHAIRS_HALO).getPaint();
+        drawCrosshairs(canvas, x, y, paint);
+        paint = DataStyle.getInternal(DataStyle.CROSSHAIRS).getPaint();
+        drawCrosshairs(canvas, x, y, paint);
     }
 
     /**
@@ -813,7 +831,7 @@ public class Map extends View implements IMapView {
      */
     private void paintZoomAndOffset(@NonNull final Canvas canvas) {
         int pos = ThemeUtils.getActionBarHeight(context) + 5 + (int) de.blau.android.layer.grid.MapOverlay.LONGTICKS_DP * 3;
-        Offset o = getBackgroundLayer().getTileLayerConfiguration().getOffset(zoomLevel);
+        Offset o = ((Main) context).getBackgroundAlignmentActionModeCallback().getLayerSource().getOffset(zoomLevel);
         String text = context.getString(R.string.zoom_and_offset, zoomLevel, o != null ? String.format(Locale.US, "%.5f", o.getDeltaLon()) : "0.00000",
                 o != null ? String.format(Locale.US, "%.5f", o.getDeltaLat()) : "0.00000");
         float textSize = textPaint.getTextSize();

src/androidTest/java/de/blau/android/imageryoffset/OffsetModeTest.java
@@ -1,9 +1,13 @@
 package de.blau.android.imageryoffset;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,13 +21,16 @@ import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject2;
 import de.blau.android.App;
 import de.blau.android.Logic;
 import de.blau.android.Main;
 import de.blau.android.Map;
 import de.blau.android.MockTileServer;
+import de.blau.android.R;
 import de.blau.android.TestUtils;
 import de.blau.android.exception.OsmException;
+import de.blau.android.layer.LayerDialogTest;
 import de.blau.android.osm.BoundingBox;
 import de.blau.android.prefs.Preferences;
 import de.blau.android.resources.TileLayerDatabase;
@@ -108,22 +115,20 @@ public class OffsetModeTest {
     @Test
     public void createOffset() {
         startMode();
-        Assert.assertTrue(TestUtils.clickText(device, false, "Align background", true, false));
-        Assert.assertTrue(TestUtils.findText(device, false, "Align background"));
         TileLayerSource tileLayerConfiguration = map.getBackgroundLayer().getTileLayerConfiguration();
         tileLayerConfiguration.setOffset(0, 0);
         TestUtils.zoomToLevel(device, main, tileLayerConfiguration.getMaxZoom());
         int zoomLevel = map.getZoomLevel();
         Offset offset = tileLayerConfiguration.getOffset(zoomLevel);
-        Assert.assertEquals(0D, offset.getDeltaLat(), 0.1E-4);
-        Assert.assertEquals(0D, offset.getDeltaLon(), 0.1E-4);
+        assertEquals(0D, offset.getDeltaLat(), 0.1E-4);
+        assertEquals(0D, offset.getDeltaLon(), 0.1E-4);
         TestUtils.drag(device, map, 8.38782, 47.390339, 8.388, 47.391, true, 50);
         TestUtils.clickOverflowButton(device);
-        TestUtils.clickText(device, false, "Save to database", true, false);
+        TestUtils.clickText(device, false, main.getString(R.string.menu_tools_background_align_save_db), true, false);
         // 74.22 m
-        TestUtils.clickText(device, false, "Cancel", true, false);
+        TestUtils.clickText(device, false, main.getString(R.string.cancel), true, false);
         TestUtils.clickOverflowButton(device);
-        TestUtils.clickText(device, false, "Apply", true, false);
+        TestUtils.clickText(device, false, main.getString(R.string.apply), true, false);
         TestUtils.clickUp(device);
         try {
             Thread.sleep(5000); // NOSONAR
@@ -131,15 +136,15 @@ public class OffsetModeTest {
         }
         zoomLevel = map.getZoomLevel();
         offset = tileLayerConfiguration.getOffset(zoomLevel);
-        Assert.assertNotNull(offset);
-        Assert.assertEquals(6.462E-4, offset.getDeltaLat(), 0.1E-4);
-        Assert.assertEquals(1.773E-4, offset.getDeltaLon(), 0.1E-4);
+        assertNotNull(offset);
+        assertEquals(6.462E-4, offset.getDeltaLat(), 0.1E-4);
+        assertEquals(1.773E-4, offset.getDeltaLon(), 0.1E-4);
     }
 
     /**
      * Zoom in and start the alignment mode
      */
-    void startMode() {
+    private void startMode() {
         TestUtils.zoomToLevel(device, main, 18);
         try {
             BoundingBox bbox = GeoMath.createBoundingBoxForCoordinates(47.390339D, 8.38782D, 50D);
@@ -152,22 +157,12 @@ public class OffsetModeTest {
             }
             main.invalidateOptionsMenu();
         } catch (OsmException e) {
-            Assert.fail(e.getMessage());
+            fail(e.getMessage());
         }
-
-        if (!TestUtils.clickMenuButton(device, "Tools", false, true)) {
-            TestUtils.clickOverflowButton(device);
-            TestUtils.clickText(device, false, "Tools", true, false);
-        }
-        if (!TestUtils.findText(device, false, "Align background")) {
-            // retry
-            device.pressBack();
-            device.waitForWindowUpdate(null, 2000);
-            if (!TestUtils.clickMenuButton(device, "Tools", false, true)) {
-                TestUtils.clickOverflowButton(device);
-                TestUtils.clickText(device, false, "Tools", true, false);
-            }
-        }
+        UiObject2 menuButton = TestUtils.getLayerButton(device, "Vespucci Test", LayerDialogTest.MENU_BUTTON);
+        menuButton.click();
+        assertTrue(TestUtils.clickText(device, false, main.getString(R.string.menu_layers_background_align), true, false));
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.menu_tools_background_align)));
     }
 
     /**
@@ -178,13 +173,11 @@ public class OffsetModeTest {
     public void downloadOffset() {
         mockServer.enqueue("imagery_offset");
         startMode();
-        Assert.assertTrue(TestUtils.clickText(device, false, "Align background", true, false));
-        Assert.assertTrue(TestUtils.findText(device, false, "Align background"));
         TileLayerSource tileLayerConfiguration = map.getBackgroundLayer().getTileLayerConfiguration();
         TestUtils.zoomToLevel(device, main, tileLayerConfiguration.getMaxZoom());
         int zoomLevel = map.getZoomLevel();
-        TestUtils.clickMenuButton(device, "From database", false, true);
-        TestUtils.clickText(device, false, "Apply", true, false);
+        TestUtils.clickMenuButton(device, main.getString(R.string.menu_tools_background_align_retrieve_from_db), false, true);
+        TestUtils.clickText(device, false, main.getString(R.string.apply), true, false);
         TestUtils.clickUp(device);
         try {
             Thread.sleep(5000); // NOSONAR
@@ -192,8 +185,8 @@ public class OffsetModeTest {
         }
         zoomLevel = map.getZoomLevel();
         Offset offset = tileLayerConfiguration.getOffset(zoomLevel);
-        Assert.assertNotNull(offset);
-        Assert.assertEquals(8.7E-6, offset.getDeltaLat(), 0.1E-6);
-        Assert.assertEquals(-1.056E-5, offset.getDeltaLon(), 0.01E-5);
+        assertNotNull(offset);
+        assertEquals(8.7E-6, offset.getDeltaLat(), 0.1E-6);
+        assertEquals(-1.056E-5, offset.getDeltaLon(), 0.01E-5);
     }
 }

src/main/java/de/blau/android/Logic.java
@@ -1617,7 +1617,7 @@ public class Logic {
     }
 
     /**
-     * Converts screen-coords to gps-coords and offests background layer.
+     * Converts screen-coords to gps-coords and offsets background layer.
      * 
      * @param screenTransX Movement on the screen.
      * @param screenTransY Movement on the screen.
@@ -1628,7 +1628,8 @@ public class Logic {
         int lat = yToLatE7(height - screenTransY);
         int relativeLon = lon - viewBox.getLeft();
         int relativeLat = lat - viewBox.getBottom();
-        TileLayerSource osmts = map.getBackgroundLayer().getTileLayerConfiguration();
+        // TileLayerSource osmts = map.getBackgroundLayer().getTileLayerConfiguration();
+        TileLayerSource osmts = ((Main) map.getContext()).getBackgroundAlignmentActionModeCallback().getLayerSource();
         double lonOffset = 0d;
         double latOffset = 0d;
         Offset o = osmts.getOffset(map.getZoomLevel());

src/main/java/de/blau/android/Main.java
@@ -1884,8 +1884,6 @@ public class Main extends FullScreenAppCompatActivity
             menu.findItem(R.id.menu_camera).setVisible(false).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         }
 
-        menu.findItem(R.id.menu_tools_background_align).setEnabled(map.getBackgroundLayer() != null);
-
         menu.findItem(R.id.menu_tools_calibrate_height)
                 .setVisible(sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null && haveTracker);
 
@@ -2402,18 +2400,6 @@ public class Main extends FullScreenAppCompatActivity
                 }
                 map.invalidate();
             });
-            return true;
-        case R.id.menu_tools_background_align:
-            // protect against weird state
-            Mode oldMode = logic.getMode() != Mode.MODE_ALIGN_BACKGROUND ? logic.getMode() : Mode.MODE_EASYEDIT;
-            try {
-                backgroundAlignmentActionModeCallback = new BackgroundAlignmentActionModeCallback(this, oldMode);
-                // NOTE needs to be after instance creation
-                logic.setMode(this, Mode.MODE_ALIGN_BACKGROUND);
-                startSupportActionMode(getBackgroundAlignmentActionModeCallback());
-            } catch (IllegalStateException isex) {
-                Log.e(DEBUG_TAG, isex.getMessage());
-            }
             return true;
         case R.id.menu_tools_apply_local_offset:
             MapTilesLayer<?> backgroundLayer = map.getBackgroundLayer();
@@ -4220,6 +4206,10 @@ public class Main extends FullScreenAppCompatActivity
      */
     public BackgroundAlignmentActionModeCallback getBackgroundAlignmentActionModeCallback() {
         return backgroundAlignmentActionModeCallback;
+    }
+
+    public void setBackgroundAlignmentActionModeCallback(BackgroundAlignmentActionModeCallback callback) {
+        backgroundAlignmentActionModeCallback = callback;
     }
 
     /**

src/main/java/de/blau/android/dialogs/Layers.java
@@ -55,12 +55,14 @@ import de.blau.android.App;
 import de.blau.android.Logic;
 import de.blau.android.Main;
 import de.blau.android.Map;
+import de.blau.android.Mode;
 import de.blau.android.R;
 import de.blau.android.contract.Paths;
 import de.blau.android.exception.OsmIllegalOperationException;
 import de.blau.android.gpx.Track;
 import de.blau.android.gpx.TrackPoint;
 import de.blau.android.gpx.WayPoint;
+import de.blau.android.imageryoffset.BackgroundAlignmentActionModeCallback;
 import de.blau.android.layer.AbstractConfigurationDialog;
 import de.blau.android.layer.ConfigureInterface;
 import de.blau.android.layer.DiscardInterface;
@@ -869,6 +871,27 @@ public class Layers extends AbstractConfigurationDialog {
                 item.setOnMenuItemClickListener(unused -> {
                     if (layer != null) {
                         BackgroundProperties.showDialog(activity, layer.getIndex());
+                    }
+                    return true;
+                });
+
+                item = menu.add(R.string.menu_layers_background_align);
+                item.setEnabled(layer.isVisible() && map.isVisible(layer));
+                item.setOnMenuItemClickListener(unused -> {
+                    if (layer != null) {
+                        try {
+                            Logic logic = App.getLogic();
+                            BackgroundAlignmentActionModeCallback backgroundAlignmentActionModeCallback = new BackgroundAlignmentActionModeCallback(
+                                    ((Main) activity), logic.getMode() != Mode.MODE_ALIGN_BACKGROUND ? logic.getMode() : Mode.MODE_EASYEDIT,
+                                    ((MapTilesLayer<?>) layer).getContentId());
+                            // NOTE needs to be after instance creation, logic.setMode needs to be called -after- this
+                            ((Main) activity).setBackgroundAlignmentActionModeCallback(backgroundAlignmentActionModeCallback);
+                            logic.setMode(((Main) activity), Mode.MODE_ALIGN_BACKGROUND);
+                            ((Main) activity).startSupportActionMode(backgroundAlignmentActionModeCallback);
+                        } catch (IllegalStateException isex) {
+                            Log.e(DEBUG_TAG, isex.getMessage());
+                        }
+                        dismissDialog();
                     }
                     return true;
                 });

src/main/java/de/blau/android/imageryoffset/BackgroundAlignmentActionModeCallback.java
@@ -83,15 +83,15 @@ public class BackgroundAlignmentActionModeCallback implements Callback {
     private static final int MENUITEM_SAVELOCAL  = 7;
     private static final int MENUITEM_HELP       = 8;
 
-    private Mode              oldMode;
+    private final Mode        oldMode;
     private final Preferences prefs;
     private final Uri         offsetServerUri;
 
     private final Offset[] oldOffsets;
 
-    private TileLayerSource osmts;
-    private final Map       map;
-    private final Main      main;
+    private final TileLayerSource osmts;
+    private final Map             map;
+    private final Main            main;
 
     private List<ImageryOffset> offsetList;
 
@@ -103,11 +103,11 @@ public class BackgroundAlignmentActionModeCallback implements Callback {
      * @param main the current instance of Main
      * @param oldMode the Mode before we were called
      */
-    public BackgroundAlignmentActionModeCallback(@NonNull Main main, @NonNull Mode oldMode) {
+    public BackgroundAlignmentActionModeCallback(@NonNull Main main, @NonNull Mode oldMode, @NonNull String layerId) {
         this.oldMode = oldMode;
         this.main = main; // currently we are only called from here
         map = main.getMap();
-        MapTilesLayer<?> layer = map.getBackgroundLayer();
+        MapTilesLayer<?> layer = (MapTilesLayer<?>) map.getLayer(layerId);
         if (layer == null) {
             throw new IllegalStateException("MapTilesLayer is null");
         }
@@ -117,6 +117,16 @@ public class BackgroundAlignmentActionModeCallback implements Callback {
         prefs = App.getPreferences(main);
         String offsetServer = prefs.getOffsetServer();
         offsetServerUri = Uri.parse(offsetServer);
+    }
+
+    /**
+     * Get the tilelayer we are currently adjusting
+     * 
+     * @return a TileLayerSource
+     */
+    @NonNull
+    public TileLayerSource getLayerSource() {
+        return osmts;
     }
 
     /**
@@ -246,6 +256,7 @@ public class BackgroundAlignmentActionModeCallback implements Callback {
 
         main.showBottomBar();
         logic.setMode(main, oldMode);
+        main.setBackgroundAlignmentActionModeCallback(null);
         main.showLock();
         main.showLayersControl();
         if (prefs.areSimpleActionsEnabled()) {

src/main/res/menu-v24/main_menu_nosubmenus.xml
@@ -173,9 +173,6 @@
         app:showAsAction="ifRoom">
         <menu>
             <item
-                android:id="@+id/menu_tools_background_align"
-                android:title="@string/menu_tools_background_align" />
-            <item
                 android:id="@+id/menu_tools_apply_local_offset"
                 android:title="@string/menu_tools_apply_local_offset" />
             <item

src/main/res/menu/main_menu.xml
@@ -176,9 +176,6 @@
         app:showAsAction="ifRoom">
         <menu>
             <item
-                android:id="@+id/menu_tools_background_align"
-                android:title="@string/menu_tools_background_align" />
-            <item
                 android:id="@+id/menu_tools_apply_local_offset"
                 android:title="@string/menu_tools_apply_local_offset" />
             <item android:title="@string/menu_tools_imagery">

commit 43da892cd05aac528597e1461c5f79c8fd5fe64c

Fix regression that caused non-downloaded areas to always be dimmed
Caused by
https://github.com/MarcusWolschon/osmeditor4android/pull/2241/commits/3b70cf8542a1ae9d702829eb0f342af60f5cecc2

Fixes https://github.com/MarcusWolschon/osmeditor4android/issues/2244

src/main/java/de/blau/android/Map.java
@@ -124,11 +124,6 @@ public class Map extends View implements IMapView {
      */
     private boolean alwaysDrawBoundingBoxes = false;
 
-    /**
-     * Locked or not
-     */
-    private boolean tmpLocked;
-
     /** cached zoom level, calculated once per onDraw pass **/
     private int zoomLevel = 0;
 
@@ -613,19 +608,20 @@ public class Map extends View implements IMapView {
         }
 
         final Logic logic = App.getLogic();
-        final boolean backgroundAlignMode = logic.getMode() == Mode.MODE_ALIGN_BACKGROUND;
-        if (zoomLevel > STORAGE_BOX_LIMIT && !backgroundAlignMode) {
+        boolean imageryAlignMode = logic.getMode() == Mode.MODE_ALIGN_BACKGROUND;
+        if (zoomLevel > STORAGE_BOX_LIMIT && !imageryAlignMode && (!logic.isLocked() || alwaysDrawBoundingBoxes)) {
             // shallow copy to avoid modification issues
             boundingBoxes.clear();
             boundingBoxes.addAll(delegator.getBoundingBoxes());
             paintStorageBox(canvas, boundingBoxes);
         }
+
         paintGpsPos(canvas);
         if (showCrosshairs && logic.isInEditZoomRange()) {
             paintCrosshairs(canvas);
         }
 
-        if (backgroundAlignMode) {
+        if (imageryAlignMode) {
             paintZoomAndOffset(canvas);
         }
 
@@ -849,43 +845,41 @@ public class Map extends View implements IMapView {
      * @param list list of bounding boxes that we've downloaded
      */
     private void paintStorageBox(@NonNull final Canvas canvas, @NonNull List<BoundingBox> list) {
-        if (!tmpLocked || alwaysDrawBoundingBoxes) {
-            Canvas c = canvas;
-            Bitmap b = null;
-            // Clipping with Op.DIFFERENCE is not supported when a device uses hardware acceleration
-            // drawing to a bitmap however will currently not be accelerated
-            final boolean noFullClipping = !hasFullClippingSupport(canvas);
-            if (noFullClipping) {
-                b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-                c = new Canvas(b);
-            } else {
-                c.save();
+        Canvas c = canvas;
+        Bitmap b = null;
+        // Clipping with Op.DIFFERENCE is not supported when a device uses hardware acceleration
+        // drawing to a bitmap however will currently not be accelerated
+        final boolean noFullClipping = !hasFullClippingSupport(canvas);
+        if (noFullClipping) {
+            b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+            c = new Canvas(b);
+        } else {
+            c.save();
+        }
+        int screenWidth = getWidth();
+        int screenHeight = getHeight();
+        ViewBox viewBox = getViewBox();
+        path.reset();
+        RectF screen = new RectF(0, 0, getWidth(), getHeight());
+        for (BoundingBox bb : list) {
+            if (bb != null && viewBox.intersects(bb)) { // only need to do this if we are on screen
+                float left = GeoMath.lonE7ToX(screenWidth, viewBox, bb.getLeft());
+                float right = GeoMath.lonE7ToX(screenWidth, viewBox, bb.getRight());
+                float bottom = GeoMath.latE7ToY(screenHeight, screenWidth, viewBox, bb.getBottom());
+                float top = GeoMath.latE7ToY(screenHeight, screenWidth, viewBox, bb.getTop());
+                RectF rect = new RectF(left, top, right, bottom);
+                rect.intersect(screen);
+                path.addRect(rect, Path.Direction.CW);
             }
-            int screenWidth = getWidth();
-            int screenHeight = getHeight();
-            ViewBox viewBox = getViewBox();
-            path.reset();
-            RectF screen = new RectF(0, 0, getWidth(), getHeight());
-            for (BoundingBox bb : list) {
-                if (bb != null && viewBox.intersects(bb)) { // only need to do this if we are on screen
-                    float left = GeoMath.lonE7ToX(screenWidth, viewBox, bb.getLeft());
-                    float right = GeoMath.lonE7ToX(screenWidth, viewBox, bb.getRight());
-                    float bottom = GeoMath.latE7ToY(screenHeight, screenWidth, viewBox, bb.getBottom());
-                    float top = GeoMath.latE7ToY(screenHeight, screenWidth, viewBox, bb.getTop());
-                    RectF rect = new RectF(left, top, right, bottom);
-                    rect.intersect(screen);
-                    path.addRect(rect, Path.Direction.CW);
-                }
-            }
+        }
 
-            c.clipPath(path, Region.Op.DIFFERENCE);
-            c.drawRect(screen, boxPaint);
+        c.clipPath(path, Region.Op.DIFFERENCE);
+        c.drawRect(screen, boxPaint);
 
-            if (noFullClipping) {
-                canvas.drawBitmap(b, 0, 0, null); // NOSONAR
-            } else {
-                c.restore();
-            }
+        if (noFullClipping) {
+            canvas.drawBitmap(b, 0, 0, null); // NOSONAR
+        } else {
+            c.restore();
         }
     }
 