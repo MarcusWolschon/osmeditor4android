9b12c79dfbaa661878d3076515a1ea87edaa7363
simonpoole <simon@poole.ch>
2023-06-01 08:54:31 +0200

    Add test

--- src/test/java/de/blau/android/LogicTest.java
+++ src/test/java/de/blau/android/LogicTest.java
@@ -3,6 +3,9 @@ package de.blau.android;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +15,7 @@ import org.robolectric.annotation.Config;
 
 import androidx.test.filters.LargeTest;
 import de.blau.android.osm.Node;
+import de.blau.android.osm.OsmElement;
 import de.blau.android.osm.Way;
 import de.blau.android.util.Util;
 
@@ -45,5 +49,26 @@ public class LogicTest {
         Node n = (Node) App.getDelegator().getOsmElement(Node.NAME, -8);
         App.getLogic().performJoinNodeToWays(null, Util.wrapInList(pan), n);
         assertEquals(5, pan.getNodes().indexOf(n));
+    }
+
+    /**
+     * Test if node gets merged and additional Node is ignored
+     * 
+     */
+    @Test
+    public void mergeNodeToWay() {
+        UnitTestUtils.loadTestData(getClass(), "join_to_way.osm");
+        Way way = (Way) App.getDelegator().getOsmElement(Way.NAME, -1);
+        assertNotNull(way);
+        main.zoomTo(way);
+        Node n1 = (Node) App.getDelegator().getOsmElement(Node.NAME, -3);
+        assertNotNull(n1);
+        Node n2 = (Node) App.getDelegator().getOsmElement(Node.NAME, -4);
+        assertNotNull(n2);
+        List<OsmElement> list = new ArrayList<>();
+        list.add(way);
+        list.add(n2);
+        App.getLogic().performJoinNodeToWays(null, list, n1);
+        assertEquals(1, way.getNodes().indexOf(n1));
     }
 }
