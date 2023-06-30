959bfe4d842bb778be6bbd4b53dcacf08568b990
simonpoole <simon@poole.ch>
2023-06-30 20:44:34 +0200

    De-support path patterns for Android pre-9.0
    See https://github.com/MarcusWolschon/osmeditor4android/issues/2298

--- src/main/java/de/blau/android/resources/DataStyle.java
+++ src/main/java/de/blau/android/resources/DataStyle.java
@@ -40,6 +40,7 @@ import android.graphics.Paint.Cap;
 import android.graphics.Paint.FontMetrics;
 import android.graphics.Paint.Join;
 import android.graphics.Paint.Style;
+import android.os.Build;
 import android.graphics.Path;
 import android.graphics.PathDashPathEffect;
 import android.graphics.Typeface;
@@ -185,6 +186,7 @@ public final class DataStyle extends DefaultHandler {
         DashPath                  dashPath       = null;
         private FontMetrics       fontMetrics    = null;
         private PathPattern       pathPattern    = null;
+        private boolean           pathPatternSupported;
         private FeatureStyle      arrowStyle     = null;
         private FeatureStyle      casingStyle    = null;
         private boolean           oneway         = false;
@@ -228,6 +230,7 @@ public final class DataStyle extends DefaultHandler {
             }
             textColor = paint.getColor();
             widthFactor = 1.0f;
+            pathPatternSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P;
         }
 
         /**
@@ -271,6 +274,7 @@ public final class DataStyle extends DefaultHandler {
             iconPath = fp.iconPath;
             cascadedStyles = null;
             textColor = fp.textColor;
+            pathPatternSupported = fp.pathPatternSupported;
         }
 
         /**
@@ -392,7 +396,7 @@ public final class DataStyle extends DefaultHandler {
                 DashPathEffect dp = new DashPathEffect(intervals, dashPath.phase);
                 paint.setPathEffect(dp);
             } else {
-                if (pathPattern != null) {
+                if (pathPattern != null && pathPatternSupported) {
                     getPaint().setPathEffect(new PathDashPathEffect(pathPattern.draw(newWidth), pathPattern.advance(newWidth), 0f, pathPattern.style()));
                 } else {
                     getPaint().setPathEffect(null);
@@ -462,7 +466,7 @@ public final class DataStyle extends DefaultHandler {
          */
         void setPathPattern(@Nullable PathPattern pathPattern) {
             this.pathPattern = pathPattern;
-            if (pathPattern != null) {
+            if (pathPattern != null && pathPatternSupported) {
                 float width = getPaint().getStrokeWidth();
                 getPaint().setPathEffect(new PathDashPathEffect(pathPattern.draw(width), pathPattern.advance(width), 0f, pathPattern.style()));
             } else {
