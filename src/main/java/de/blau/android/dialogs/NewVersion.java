e8df919aabf923a64acab99d725530310abc5a18
simonpoole <simon@poole.ch>
2024-02-29 15:16:25 +0100

    Suggest migration to OAuth 2 when a new version is installed

--- src/main/java/de/blau/android/dialogs/NewVersion.java
+++ src/main/java/de/blau/android/dialogs/NewVersion.java
@@ -1,17 +1,30 @@
 package de.blau.android.dialogs;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatDialog;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import de.blau.android.App;
+import de.blau.android.Authorize;
 import de.blau.android.HelpViewer;
 import de.blau.android.R;
+import de.blau.android.contract.Urls;
 import de.blau.android.listener.DoNothingListener;
+import de.blau.android.osm.Server;
+import de.blau.android.prefs.API;
+import de.blau.android.prefs.API.Auth;
+import de.blau.android.prefs.AdvancedPrefDatabase;
+import de.blau.android.prefs.Preferences;
 import de.blau.android.util.ImmersiveDialogFragment;
+import de.blau.android.util.Util;
 
 /**
  * Display a dialog displaying information on a new version and offering to display the release notes.
@@ -56,21 +69,43 @@ public class NewVersion extends ImmersiveDialogFragment {
     @NonNull
     private static NewVersion newInstance() {
         NewVersion f = new NewVersion();
-
         f.setShowsDialog(true);
-
         return f;
     }
 
     @NonNull
     @Override
     public AppCompatDialog onCreateDialog(Bundle savedInstanceState) {
+        final FragmentActivity activity = getActivity();
         Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.upgrade_title);
-        builder.setMessage(R.string.upgrade_message);
-        DoNothingListener doNothingListener = new DoNothingListener();
-        builder.setNegativeButton(R.string.skip, doNothingListener);
-        builder.setNeutralButton(R.string.read_upgrade, (dialog, which) -> HelpViewer.start(getActivity(), R.string.help_upgrade));
-        return builder.create();
+        // shoe-horned OAuth 2 migration
+        Preferences prefs = App.getPreferences(getContext());
+        Server server = prefs.getServer();
+        String message = getString(R.string.upgrade_message);
+        if (server.getAuthentication() != Auth.OAUTH2 && Urls.DEFAULT_API_NAME.equals(server.getApiName())) {
+            // migration necessary
+            builder.setPositiveButton(R.string.migrate_now, null);
+            message += getString(R.string.upgrade_message_oauth2);
+        }
+        builder.setMessage(Util.fromHtml(message));
+        builder.setNegativeButton(R.string.skip, (d, which) -> dismiss());
+        builder.setNeutralButton(R.string.read_upgrade, (d, which) -> {
+            dismiss();
+            HelpViewer.start(activity, R.string.help_upgrade);
+        });
+        final AlertDialog dialog = builder.create();
+        dialog.setOnShowListener((DialogInterface d) -> {
+            Button positive = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+            positive.setOnClickListener((View v) -> {
+                try (AdvancedPrefDatabase db = new AdvancedPrefDatabase(activity)) {
+                    API api = db.getCurrentAPI();
+                    db.setAPIDescriptors(api.id, api.name, api.url, api.readonlyurl, api.notesurl, Auth.OAUTH2);
+                }
+                Authorize.startForResult(activity, null);
+                positive.setEnabled(false);
+            });
+        });
+        return dialog;
     }
 }
