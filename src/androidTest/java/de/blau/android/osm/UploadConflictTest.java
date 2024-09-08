bae6477b873ad6960bebeba5de50e93d0ff4ab03
simonpoole <simon@poole.ch>
2024-09-08 10:43:58 +0200

    Support conflict resolution for partial uploads

--- src/androidTest/java/de/blau/android/osm/UploadConflictTest.java
+++ src/androidTest/java/de/blau/android/osm/UploadConflictTest.java
@@ -111,7 +111,7 @@ public class UploadConflictTest {
      */
     @Test
     public void versionConflictUseLocal() {
-        versionConflict("conflict1", new String[] { "conflictdownload1" }, false, R.string.upload_conflict_message_version);
+        conflict("conflict1", new String[] { "conflictdownload1" }, false, R.string.upload_conflict_message_version);
         assertTrue(TestUtils.clickText(device, false, main.getString(R.string.resolve), true));
         assertTrue(TestUtils.clickText(device, false, main.getString(R.string.use_local_version), true));
         assertTrue(TestUtils.findText(device, false, main.getString(R.string.confirm_upload_title), 5000));
@@ -120,11 +120,40 @@ public class UploadConflictTest {
     }
 
     /**
+     * Version conflict use the local element, just uploading a selection
+     */
+    @Test
+    public void versionConflictUseLocalSelection() {
+        loadDataAndFixtures("conflict1", new String[] { "conflictdownload1" }, false);
+
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        TestUtils.unlock(device);
+        Node n = (Node) App.getDelegator().getOsmElement(Node.NAME, 101792984L);
+        TestUtils.clickAtCoordinates(device, main.getMap(), n.getLon(), n.getLat());
+
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.actionmode_nodeselect), 10000));
+
+        assertTrue(TestUtils.clickOverflowButton(device));
+        TestUtils.scrollTo(main.getString(R.string.menu_upload_element), false);
+        assertTrue(TestUtils.clickText(device, false, main.getString(R.string.menu_upload_element), true));
+
+        uploadDialog(R.string.upload_conflict_message_version, device);
+
+        assertTrue(TestUtils.clickText(device, false, main.getString(R.string.resolve), true));
+        assertTrue(TestUtils.clickText(device, false, main.getString(R.string.use_local_version), true));
+        assertTrue(TestUtils.findText(device, false, main.getString(R.string.confirm_upload_title), 5000));
+        assertFalse(TestUtils.findText(device, false, "Kindhauserstrasse"));
+        n = (Node) App.getDelegator().getOsmElement(Node.NAME, 101792984L);
+        assertEquals(7, n.getOsmVersion()); // version should now be the same as the server
+    }
+
+    /**
      * Version conflict use the server element
      */
     @Test
     public void versionConflictUseServer() {
-        versionConflict("conflict1", new String[] { "conflictdownload1" }, false, R.string.upload_conflict_message_version);
+        conflict("conflict1", new String[] { "conflictdownload1" }, false, R.string.upload_conflict_message_version);
         Node n = (Node) App.getDelegator().getOsmElement(Node.NAME, 101792984L);
         assertNotNull(n);
         assertEquals(6, n.getOsmVersion()); // version should now be server and not in the API
@@ -151,7 +180,7 @@ public class UploadConflictTest {
      */
     @Test
     public void severElementAlreadyDeleted() {
-        versionConflict("conflict2", new String[] { "410", "200" }, false, -1);
+        conflict("conflict2", new String[] { "410", "200" }, false, -1);
         try {
             final MockWebServer server = mockServer.server();
             server.takeRequest(10L, TimeUnit.SECONDS);
@@ -171,11 +200,12 @@ public class UploadConflictTest {
      */
     @Test
     public void severElementInUse() {
-        versionConflict("conflict3", new String[] { "way-210461100", "way-210461100-nodes", "relation-12345", "relation-12345", "empty" }, false, -1);
+        conflict("conflict3", new String[] { "way-210461100", "way-210461100-nodes", "relation-12345", "relation-12345", "empty" }, false, -1);
         Way w = App.getDelegator().getApiStorage().getWay(210461100L);
         assertNotNull(w);
         assertEquals(OsmElement.STATE_DELETED, w.getState());
 
+        assertTrue(TestUtils.findText(device, false, "12345", 10000, true));
         assertTrue(TestUtils.clickText(device, false, main.getString(R.string.resolve), true));
         assertTrue(TestUtils.clickText(device, false, main.getString(R.string.deleting_references_on_server), true));
         assertTrue(TestUtils.findText(device, false, main.getString(R.string.confirm_upload_title), 20000));
@@ -191,21 +221,66 @@ public class UploadConflictTest {
      */
     @Test
     public void referencesMissing() {
-        versionConflict("conflict4", new String[] { "way-27009604", "way-27009604-nodes", "nodes-deleted" }, false,
+        conflict("conflict4", new String[] { "way-27009604", "way-27009604-nodes", "nodes-deleted" }, false,
                 R.string.upload_conflict_message_missing_references);
         Way w = App.getDelegator().getApiStorage().getWay(27009604L);
         assertTrue(TestUtils.clickText(device, false, main.getString(R.string.cancel), true));
     }
 
     /**
-     * Upload to changes (mock-)server and wait for version conflict dialog
+     * Upload to changes (mock-)server and wait for conflict dialog
      * 
      * @param conflictReponse the response
      * @param fixtures name of additional fixtures with the response to the upload
      * @param userDetails if true enqueue user details
-     * @param waitForDialog wait for the conflict dialog if true
+     * @param titleRes title to expect
      */
-    private void versionConflict(@NonNull String conflictReponse, @NonNull String[] fixtures, boolean userDetails, int titleRes) {
+    private void conflict(@NonNull String conflictReponse, @NonNull String[] fixtures, boolean userDetails, int titleRes) {
+        loadDataAndFixtures(conflictReponse, fixtures, userDetails);
+
+        TestUtils.clickMenuButton(device, main.getString(R.string.menu_transfer), false, true);
+
+        TestUtils.clickText(device, false, main.getString(R.string.menu_transfer_upload), true, false); // menu item
+
+        uploadDialog(titleRes, device);
+    }
+
+    /**
+     * FIllout comment and source fields and upload
+     * 
+     * @param titleRes title to expect
+     * @param device UiDevice
+     */
+    private void uploadDialog(int titleRes, UiDevice device) {
+        UiSelector uiSelector = new UiSelector().resourceId("android:id/button1"); // dialog upload button
+        UiObject button = device.findObject(uiSelector);
+        try {
+            button.click();
+        } catch (UiObjectNotFoundException e1) {
+            fail(e1.getMessage());
+        }
+        fillCommentAndSource(instrumentation, device);
+        TestUtils.sleep();
+        uiSelector = new UiSelector().resourceId("android:id/button1");
+        button = device.findObject(uiSelector);
+        try {
+            button.clickAndWaitForNewWindow();
+        } catch (UiObjectNotFoundException e1) {
+            fail(e1.getMessage());
+        }
+        if (titleRes > 0) {
+            assertTrue(TestUtils.findText(device, false, main.getString(titleRes), 10000));
+        }
+    }
+
+    /**
+     * Load the data and fixtures
+     * 
+     * @param conflictReponse the response
+     * @param fixtures name of additional fixtures with the response to the upload
+     * @param userDetails if true enqueue user details
+     */
+    private void loadDataAndFixtures(String conflictReponse, String[] fixtures, boolean userDetails) {
         final CountDownLatch signal = new CountDownLatch(1);
         Logic logic = App.getLogic();
 
@@ -223,28 +298,6 @@ public class UploadConflictTest {
         }
         for (String fixture : fixtures) {
             mockServer.enqueue(fixture);
-        }
-
-        TestUtils.clickMenuButton(device, main.getString(R.string.menu_transfer), false, true);
-        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-
-        TestUtils.clickText(device, false, main.getString(R.string.menu_transfer_upload), true, false); // menu item
-
-        UiSelector uiSelector = new UiSelector().className("android.widget.Button").instance(1); // dialog upload button
-        UiObject button = device.findObject(uiSelector);
-        try {
-            button.click();
-        } catch (UiObjectNotFoundException e1) {
-            fail(e1.getMessage());
-        }
-        fillCommentAndSource(instrumentation, device);
-        try {
-            button.clickAndWaitForNewWindow();
-        } catch (UiObjectNotFoundException e1) {
-            fail(e1.getMessage());
-        }
-        if (titleRes > 0) {
-            assertTrue(TestUtils.findText(device, false, main.getString(titleRes), 10000));
         }
     }
 
