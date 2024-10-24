MarcusWolschon/osmeditor4android

commit 5c6ad694acda6e16272c84ec4dbb9a3f96201215

Support match expressions for preset items and fields
Fixes https://github.com/MarcusWolschon/osmeditor4android/issues/2651

src/main/java/de/blau/android/Logic.java
@@ -1627,7 +1627,7 @@ public class Logic {
      */
     private void updateDirection(@NonNull Context context, float angle, int direction, @NonNull final Node node) {
         // this is obviously quiet expensive bit avoids having state somewhere else
-        String directionKey = Tags.getDirectionKey(Preset.findBestMatch(App.getCurrentPresets(context), node.getTags(), null, null), node);
+        String directionKey = Tags.getDirectionKey(Preset.findBestMatch(context, App.getCurrentPresets(context), node.getTags(), null, node), node);
         if (directionKey != null) {
             java.util.Map<String, String> tags = new HashMap<>(node.getTags());
             Float currentAngle = Tags.parseDirection(tags.get(directionKey));

src/main/java/de/blau/android/easyedit/NodeSelectionActionModeCallback.java
@@ -38,6 +38,7 @@ import de.blau.android.osm.Result;
 import de.blau.android.osm.Tags;
 import de.blau.android.osm.Way;
 import de.blau.android.presets.Preset;
+import de.blau.android.util.GeoContext;
 import de.blau.android.util.GeoMath;
 import de.blau.android.util.ScreenMessage;
 import de.blau.android.util.Sound;
@@ -59,6 +60,7 @@ public class NodeSelectionActionModeCallback extends ElementSelectionActionModeC
     private static final int MENUITEM_ADDRESS      = LAST_REGULAR_MENUITEM + 7;
     private static final int MENUITEM_ROTATE       = LAST_REGULAR_MENUITEM + 8;
 
+    private final GeoContext geoContext;
     private List<OsmElement> joinableElements = null;
     private List<Way>        appendableWays   = null;
     private List<Way>        highways         = new ArrayList<>();
@@ -79,6 +81,7 @@ public class NodeSelectionActionModeCallback extends ElementSelectionActionModeC
      */
     NodeSelectionActionModeCallback(EasyEditManager manager, Node node) {
         super(manager, node);
+        geoContext = App.getGeoContext(main);
     }
 
     @Override
@@ -139,8 +142,9 @@ public class NodeSelectionActionModeCallback extends ElementSelectionActionModeC
             }
         }
         updated |= setItemVisibility(highways.size() >= 2, restrictionItem, false);
-
-        updated |= setItemVisibility(Tags.getDirectionKey(Preset.findBestMatch(presets, element.getTags(), null, null), element) != null, rotateItem, false);
+        updated |= setItemVisibility(Tags.getDirectionKey(
+                Preset.findBestMatch(main, presets, element.getTags(), geoContext != null ? geoContext.getIsoCodes(element) : null, element), element) != null,
+                rotateItem, false);
 
         if (updated) {
             arrangeMenu(menu);

src/main/java/de/blau/android/layer/data/MapOverlay.java
@@ -1172,7 +1172,7 @@ public class MapOverlay<O extends OsmElement> extends MapViewLayer
         java.util.Map<String, String> tags = e.getTags();
         PresetItem match = matchCache.get(tags);
         if (match == null && !matchCache.containsKey(tags)) {
-            match = Preset.findBestMatch(tmpPresets, tags, null, null);
+            match = Preset.findBestMatch(context, tmpPresets, e.getTags(), null, e);
             matchCache.put(tags, match);
         }
         return match;

src/main/java/de/blau/android/presets/Preset.java
@@ -38,17 +38,20 @@ import android.util.Log;
 import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import ch.poole.osm.josmfilterparser.Condition;
 import ch.poole.poparser.Po;
 import de.blau.android.App;
 import de.blau.android.R;
 import de.blau.android.contract.FileExtensions;
 import de.blau.android.contract.Paths;
 import de.blau.android.osm.DiscardedTags;
+import de.blau.android.osm.OsmElement;
 import de.blau.android.osm.OsmElement.ElementType;
 import de.blau.android.osm.OsmXml;
 import de.blau.android.osm.Tags;
 import de.blau.android.prefs.AdvancedPrefDatabase;
 import de.blau.android.prefs.PresetEditorActivity;
+import de.blau.android.search.Wrapper;
 import de.blau.android.util.Hash;
 import de.blau.android.util.SavingHelper;
 import de.blau.android.util.SearchIndexUtils;
@@ -99,6 +102,11 @@ public class Preset implements Serializable {
 
     // hardwired layout stuff
     public static final int SPACING = 5;
+
+    /**
+     * Global condition cache
+     */
+    private static Map<String, Condition> conditionCache = new HashMap<>();
 
     /** The directory containing all data (xml, MRU data, images) about this preset */
     private File directory;
@@ -944,7 +952,7 @@ public class Preset implements Serializable {
         PresetGroup recent = new PresetGroup(dummy, null, "recent", null);
         recent.setItemSort(false);
         PresetMRUInfo.addToPresetGroup(recent, presets, regions);
-        return recent.getGroupView(ctx, handler, type, null, null); // we've already filtered on region
+        return recent.getGroupView(ctx, handler, type, null, null, null); // we've already filtered on region
     }
 
     /**
@@ -1055,6 +1063,48 @@ public class Preset implements Serializable {
     }
 
     /**
+     * Finds the preset item best matching an OSM element, or null if no preset item matches. To match, all (mandatory)
+     * tags of the preset item need to be in the tag set. The preset item does NOT need to have all tags in the tag set,
+     * but the tag set needs to have all (mandatory) tags of the preset item.
+     * 
+     * If multiple items match, the most specific one (i.e. having most tags) wins. If there is a draw, no guarantees
+     * are made.
+     * 
+     * This version takes any match_expression attributes in the candidate presets in to account
+     * 
+     * @param context an Android context
+     * @param presets presets to match against
+     * @param tags tags to check against (i.e. tags of a map element)
+     * @param regions if not null this will be taken in to account wrt scoring
+     * @param osmElement
+     * @return @return null, or the "best" matching item for the element
+     */
+    public static PresetItem findBestMatch(@NonNull Context context, @Nullable Preset[] presets, @Nullable Map<String, String> tags,
+            @Nullable List<String> regions, @NonNull OsmElement osmElement) {
+        if (tags == null || presets == null) {
+            Log.e(DEBUG_TAG, "findBestMatch " + (tags == null ? "tags null" : "presets null"));
+            return null;
+        }
+        // Build candidate list
+        Set<PresetItem> possibleMatches = new LinkedHashSet<>();
+        buildPossibleMatches(possibleMatches, presets, tags, false, null);
+        // check match expressions
+        Wrapper wrapper = new Wrapper(context);
+        wrapper.setElement(osmElement);
+        for (PresetItem candidate : new ArrayList<>(possibleMatches)) {
+            String matchExpression = candidate.getMatchExpression();
+            if (matchExpression != null) {
+                Condition condition = de.blau.android.search.Util.getCondition(conditionCache, matchExpression);
+                wrapper.setElement(osmElement);
+                if (condition != null && !condition.eval(Wrapper.toJosmFilterType(osmElement), wrapper, tags)) {
+                    possibleMatches.remove(candidate);
+                }
+            }
+        }
+        return findBestMatch(tags, regions, null, possibleMatches);
+    }
+
+    /**
      * Finds the preset item best matching a certain tag set, or null if no preset item matches. To match, all
      * (mandatory) tags of the preset item need to be in the tag set. The preset item does NOT need to have all tags in
      * the tag set, but the tag set needs to have all (mandatory) tags of the preset item.
@@ -1097,9 +1147,6 @@ public class Preset implements Serializable {
     @Nullable
     public static PresetItem findBestMatch(@Nullable Preset[] presets, @Nullable Map<String, String> tags, @Nullable List<String> regions,
             @Nullable ElementType elementType, boolean useAddressKeys, @Nullable Map<String, String> ignoreTags) {
-        int bestMatchStrength = 0;
-        PresetItem bestMatch = null;
-
         if (tags == null || presets == null) {
             Log.e(DEBUG_TAG, "findBestMatch " + (tags == null ? "tags null" : "presets null"));
             return null;
@@ -1112,6 +1159,27 @@ public class Preset implements Serializable {
         if (useAddressKeys && possibleMatches.isEmpty()) {
             buildPossibleMatches(possibleMatches, presets, tags, true, ignoreTags);
         }
+
+        return findBestMatch(tags, regions, elementType, possibleMatches);
+    }
+
+    /**
+     * Finds the preset item best matching a certain tag set, or null if no preset item matches. To match, all
+     * (mandatory) tags of the preset item need to be in the tag set. The preset item does NOT need to have all tags in
+     * the tag set, but the tag set needs to have all (mandatory) tags of the preset item.
+     * 
+     * If multiple items match, the most specific one (i.e. having most tags) wins. If there is a draw, no guarantees
+     * are made.
+     * 
+     * @param tags tags to check against (i.e. tags of a map element)
+     * @param regions if not null this will be taken in to account wrt scoring
+     * @param elementType if not null the ElementType will be considered
+     * @param possibleMatches candidate matches
+     * @return the best match or null
+     */
+    private static PresetItem findBestMatch(Map<String, String> tags, List<String> regions, ElementType elementType, Set<PresetItem> possibleMatches) {
+        int bestMatchStrength = 0;
+        PresetItem bestMatch = null;
         // Find best
         // always prioritize presets with fixed keys that match
         for (PresetItem possibleMatch : possibleMatches) {
@@ -1213,12 +1281,28 @@ public class Preset implements Serializable {
      * @return a filtered list containing only elements of the specified type
      */
     @NonNull
-    static List<PresetElement> filterElements(@NonNull List<PresetElement> originalElements, @NonNull ElementType type) {
+    static List<PresetElement> filterElements(@NonNull List<PresetElement> originalElements, @Nullable ElementType type, @Nullable OsmElement osmElement) {
         List<PresetElement> filteredElements = new ArrayList<>();
+        Wrapper wrapper = null;
+        Map<String, String> tags = null;
         for (PresetElement e : originalElements) {
             if (!e.isDeprecated() && (e.appliesTo(type) || ((e instanceof PresetSeparator) && !filteredElements.isEmpty()
                     && !(filteredElements.get(filteredElements.size() - 1) instanceof PresetSeparator)))) {
                 // only add separators if there is a non-separator element above them
+                if (osmElement != null && e instanceof PresetItem) {
+                    String matchExpression = ((PresetItem) e).getMatchExpression();
+                    if (matchExpression != null) {
+                        if (wrapper == null) {
+                            wrapper = new Wrapper();
+                            wrapper.setElement(osmElement);
+                            tags = osmElement.getTags();
+                        }
+                        Condition condition = de.blau.android.search.Util.getCondition(conditionCache, matchExpression);
+                        if (condition != null && !condition.eval(Wrapper.toJosmFilterType(osmElement), wrapper, tags)) {
+                            continue;
+                        }
+                    }
+                }
                 filteredElements.add(e);
             }
         }

src/main/java/de/blau/android/propertyeditor/TagEditorFragment.java
@@ -669,14 +669,14 @@ public class TagEditorFragment extends SelectableRowsFragment implements Propert
         clearSecondaryPresets();
         final List<String> regions = propertyEditorListener.getIsoCodes();
         if (presetItem == null) {
-            primaryPresetItem = Preset.findBestMatch(presets, allTags, regions, null, true, null);
+            primaryPresetItem = Preset.findBestMatch(getContext(), presets, allTags, regions, elements[0]);
         } else {
             primaryPresetItem = presetItem;
         }
         Map<String, String> nonAssigned = addPresetsToTags(primaryPresetItem, allTags);
         int nonAssignedCount = nonAssigned.size();
         while (nonAssignedCount > 0) {
-            PresetItem nonAssignedPreset = Preset.findBestMatch(presets, nonAssigned, regions, null, true, null);
+            PresetItem nonAssignedPreset = Preset.findBestMatch(getContext(), presets, nonAssigned, regions, elements[0]);
             if (nonAssignedPreset == null) {
                 // no point in continuing
                 break;

src/main/java/de/blau/android/easyedit/EditRelationMembersActionModeCallback.java
@@ -2,7 +2,6 @@ package de.blau.android.easyedit;
 
 import static de.blau.android.contract.Constants.LOG_TAG_LEN;
 
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -20,7 +19,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.view.ActionMode;
 import ch.poole.osm.josmfilterparser.Condition;
-import ch.poole.osm.josmfilterparser.JosmFilterParser;
 import de.blau.android.App;
 import de.blau.android.R;
 import de.blau.android.exception.OsmIllegalOperationException;
@@ -442,7 +440,7 @@ public class EditRelationMembersActionModeCallback extends BuilderActionModeCall
                     }
                     memberExpression = memberExpression.trim();
                     wrapper.setElement(e);
-                    Condition condition = getCondition(conditionCache, memberExpression);
+                    Condition condition = de.blau.android.search.Util.getCondition(conditionCache, memberExpression);
                     if (condition != null && condition.eval(Wrapper.toJosmFilterType(e), wrapper, e.getTags())) {
                         clickable.add(e);
                         break;
@@ -452,34 +450,6 @@ public class EditRelationMembersActionModeCallback extends BuilderActionModeCall
         }
         logic.setClickableElements(clickable);
 
-    }
-
-    /**
-     * Get the Condition object for a memberExpression, caching it
-     * 
-     * @param conditionCache the cache
-     * @param memberExpression the
-     * @return the Condition or null
-     */
-    @Nullable
-    private Condition getCondition(@NonNull Map<String, Condition> conditionCache, @NonNull String memberExpression) {
-        Condition condition = conditionCache.get(memberExpression); // NOSONAR
-        if (condition == null) {
-            try {
-                JosmFilterParser parser = new JosmFilterParser(new ByteArrayInputStream(memberExpression.getBytes()));
-                condition = parser.condition();
-            } catch (ch.poole.osm.josmfilterparser.ParseException | IllegalArgumentException ex) {
-                Log.e(DEBUG_TAG, "member_expression " + memberExpression + " caused " + ex.getMessage());
-                try {
-                    JosmFilterParser parser = new JosmFilterParser(new ByteArrayInputStream("".getBytes()));
-                    condition = parser.condition();
-                } catch (ch.poole.osm.josmfilterparser.ParseException | IllegalArgumentException ex2) {
-                    Log.e(DEBUG_TAG, "member_expression dummy caused " + ex2.getMessage());
-                }
-            }
-            conditionCache.put(memberExpression, condition);
-        }
-        return condition;
     }
 
     /**

src/main/java/de/blau/android/filter/PresetFilterActivity.java
@@ -98,7 +98,7 @@ public class PresetFilterActivity extends ConfigurationChangeAwareActivity imple
      * @return a View of the PresetGroup
      */
     private ScrollView getPresetView(@NonNull PresetGroup group, @Nullable PresetElement element) {
-        View view = group.getGroupView(this, this, null, element, null);
+        View view = group.getGroupView(this, this, null, null, element, null);
         view.setId(R.id.preset_view);
         return (ScrollView) view;
     }
@@ -146,7 +146,7 @@ public class PresetFilterActivity extends ConfigurationChangeAwareActivity imple
         case R.id.preset_menu_top:
             if (rootGroup != null) {
                 currentGroup = rootGroup;
-                currentGroup.getGroupView(this, presetView, this, null, filter.getPresetElement(), null);
+                currentGroup.getGroupView(this, presetView, this, null, null, filter.getPresetElement(), null);
                 presetView.invalidate();
                 invalidateOptionsMenu();
                 return true;
@@ -157,7 +157,7 @@ public class PresetFilterActivity extends ConfigurationChangeAwareActivity imple
                 PresetGroup group = currentGroup.getParent();
                 if (group != null) {
                     currentGroup = group;
-                    currentGroup.getGroupView(this, presetView, this, null, filter.getPresetElement(), null);
+                    currentGroup.getGroupView(this, presetView, this, null, null, filter.getPresetElement(), null);
                     presetView.invalidate();
                     invalidateOptionsMenu();
                     return true;
@@ -211,7 +211,7 @@ public class PresetFilterActivity extends ConfigurationChangeAwareActivity imple
         }
         filter = (PresetFilter) App.getLogic().getFilter();
         currentGroup = group;
-        currentGroup.getGroupView(this, presetView, this, null, filter.getPresetElement(), null);
+        currentGroup.getGroupView(this, presetView, this, null, null, filter.getPresetElement(), null);
         presetView.invalidate();
         invalidateOptionsMenu();
     }
@@ -235,7 +235,7 @@ public class PresetFilterActivity extends ConfigurationChangeAwareActivity imple
             final PresetElementPath path = element.getPath(rootGroup);
             if (path != null) {
                 filter.setPresetElement(path);
-                currentGroup.getGroupView(this, presetView, this, null, element, null);
+                currentGroup.getGroupView(this, presetView, this, null, null, element, null);
             }
             presetView.invalidate();
         }

src/main/java/de/blau/android/presets/MRUTags.java
@@ -1,5 +1,7 @@
 package de.blau.android.presets;
 
+import static de.blau.android.contract.Constants.LOG_TAG_LEN;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -48,7 +50,9 @@ import de.blau.android.util.collections.MRUList;
  *
  */
 public class MRUTags {
-    private static final String DEBUG_TAG = MRUTags.class.getSimpleName().substring(0, Math.min(23, MRUTags.class.getSimpleName().length()));
+
+    private static final int    TAG_LEN   = Math.min(LOG_TAG_LEN, MRUTags.class.getSimpleName().length());
+    private static final String DEBUG_TAG = MRUTags.class.getSimpleName().substring(0, TAG_LEN);
 
     private static final String MRUTAGS_TAG      = "mrutags";
     private static final String VALUE_TAG        = "value";
@@ -66,11 +70,11 @@ public class MRUTags {
     private static final int VALUE_MRU_SIZE = 10;
     private static final int ROLE_MRU_SIZE  = 10;
 
-    Map<PresetItem, Map<String, MRUList<String>>> valueStore = new HashMap<>();
-    Map<ElementType, MRUList<String>>             keyStore   = new EnumMap<>(ElementType.class);
-    Map<PresetItem, MRUList<String>>              roleStore  = new HashMap<>();
-    final PresetItem                              dummyItem;
-    boolean                                       dirty      = false;
+    private Map<PresetItem, Map<String, MRUList<String>>> valueStore = new HashMap<>();
+    private Map<ElementType, MRUList<String>>             keyStore   = new EnumMap<>(ElementType.class);
+    private Map<PresetItem, MRUList<String>>              roleStore  = new HashMap<>();
+    private final PresetItem                              dummyItem;
+    private boolean                                       dirty      = false;
 
     /**
      * Construct a new container for most recently used tags
@@ -341,16 +345,17 @@ public class MRUTags {
             serializer.startTag("", VALUES_TAG);
             writePathXML(ctx, pathCache, serializer, entry.getKey());
             for (Entry<String, MRUList<String>> entry2 : entry.getValue().entrySet()) {
-                if (entry2 != null) {
-                    serializer.startTag("", KEY_TAG);
-                    serializer.attribute("", VALUE_ATTR, entry2.getKey());
-                    for (String v : entry2.getValue()) {
-                        serializer.startTag("", VALUE_TAG);
-                        serializer.attribute("", VALUE_ATTR, v);
-                        serializer.endTag("", VALUE_TAG);
-                    }
-                    serializer.endTag("", KEY_TAG);
+                if (entry2 == null) {
+                    continue;
                 }
+                serializer.startTag("", KEY_TAG);
+                serializer.attribute("", VALUE_ATTR, entry2.getKey());
+                for (String v : entry2.getValue()) {
+                    serializer.startTag("", VALUE_TAG);
+                    serializer.attribute("", VALUE_ATTR, v);
+                    serializer.endTag("", VALUE_TAG);
+                }
+                serializer.endTag("", KEY_TAG);
             }
             serializer.endTag("", VALUES_TAG);
         }
@@ -380,7 +385,8 @@ public class MRUTags {
      * @param item the PresetItem
      * @throws IOException it something went wrong while writing
      */
-    private void writePathXML(Context ctx, Map<PresetItem, PresetElementPath> pathCache, XmlSerializer serializer, PresetItem item) throws IOException {
+    private void writePathXML(@NonNull Context ctx, @NonNull Map<PresetItem, PresetElementPath> pathCache, @NonNull XmlSerializer serializer,
+            @NonNull PresetItem item) throws IOException {
         serializer.startTag("", PRESET_TAG);
         PresetElementPath path = pathCache.get(item); // NOSONAR needs Android 24
         if (path == null) {

commit 61f457e585e12bcbdaa2c20ac2d9f46f90aeada7

Fix regression that led to address fields no longer being matched
Regression introduced in 5c6ad694acda6e16272c84ec4dbb9a3f96201215

src/main/java/de/blau/android/Logic.java
@@ -1626,8 +1626,8 @@ public class Logic {
      * @param node the Node
      */
     private void updateDirection(@NonNull Context context, float angle, int direction, @NonNull final Node node) {
-        // this is obviously quiet expensive bit avoids having state somewhere else
-        String directionKey = Tags.getDirectionKey(Preset.findBestMatch(context, App.getCurrentPresets(context), node.getTags(), null, node), node);
+        // this is obviously rather expensive bit avoids having state somewhere else
+        String directionKey = Tags.getDirectionKey(Preset.findBestMatch(context, App.getCurrentPresets(context), node.getTags(), null, node, false), node);
         if (directionKey != null) {
             java.util.Map<String, String> tags = new HashMap<>(node.getTags());
             Float currentAngle = Tags.parseDirection(tags.get(directionKey));

src/main/java/de/blau/android/easyedit/NodeSelectionActionModeCallback.java
@@ -143,8 +143,8 @@ public class NodeSelectionActionModeCallback extends ElementSelectionActionModeC
         }
         updated |= setItemVisibility(highways.size() >= 2, restrictionItem, false);
         updated |= setItemVisibility(Tags.getDirectionKey(
-                Preset.findBestMatch(main, presets, element.getTags(), geoContext != null ? geoContext.getIsoCodes(element) : null, element), element) != null,
-                rotateItem, false);
+                Preset.findBestMatch(main, presets, element.getTags(), geoContext != null ? geoContext.getIsoCodes(element) : null, element, false),
+                element) != null, rotateItem, false);
 
         if (updated) {
             arrangeMenu(menu);

src/main/java/de/blau/android/layer/data/MapOverlay.java
@@ -1172,7 +1172,7 @@ public class MapOverlay<O extends OsmElement> extends MapViewLayer
         java.util.Map<String, String> tags = e.getTags();
         PresetItem match = matchCache.get(tags);
         if (match == null && !matchCache.containsKey(tags)) {
-            match = Preset.findBestMatch(context, tmpPresets, e.getTags(), null, e);
+            match = Preset.findBestMatch(context, tmpPresets, e.getTags(), null, e, false);
             matchCache.put(tags, match);
         }
         return match;

src/main/java/de/blau/android/presets/Preset.java
@@ -1076,11 +1076,12 @@ public class Preset implements Serializable {
      * @param presets presets to match against
      * @param tags tags to check against (i.e. tags of a map element)
      * @param regions if not null this will be taken in to account wrt scoring
-     * @param osmElement
+     * @param osmElement the OsmElement
+     * @param useAddressKeys use addr: keys if true
      * @return @return null, or the "best" matching item for the element
      */
     public static PresetItem findBestMatch(@NonNull Context context, @Nullable Preset[] presets, @Nullable Map<String, String> tags,
-            @Nullable List<String> regions, @NonNull OsmElement osmElement) {
+            @Nullable List<String> regions, @NonNull OsmElement osmElement, boolean useAddressKeys) {
         if (tags == null || presets == null) {
             Log.e(DEBUG_TAG, "findBestMatch " + (tags == null ? "tags null" : "presets null"));
             return null;
@@ -1088,6 +1089,9 @@ public class Preset implements Serializable {
         // Build candidate list
         Set<PresetItem> possibleMatches = new LinkedHashSet<>();
         buildPossibleMatches(possibleMatches, presets, tags, false, null);
+        if (useAddressKeys && possibleMatches.isEmpty()) { // use address keys
+            buildPossibleMatches(possibleMatches, presets, tags, true, null);
+        }
         // check match expressions
         Wrapper wrapper = new Wrapper(context);
         wrapper.setElement(osmElement);

src/main/java/de/blau/android/propertyeditor/TagEditorFragment.java
@@ -669,14 +669,14 @@ public class TagEditorFragment extends SelectableRowsFragment implements Propert
         clearSecondaryPresets();
         final List<String> regions = propertyEditorListener.getIsoCodes();
         if (presetItem == null) {
-            primaryPresetItem = Preset.findBestMatch(getContext(), presets, allTags, regions, elements[0]);
+            primaryPresetItem = Preset.findBestMatch(getContext(), presets, allTags, regions, elements[0], true);
         } else {
             primaryPresetItem = presetItem;
         }
         Map<String, String> nonAssigned = addPresetsToTags(primaryPresetItem, allTags);
         int nonAssignedCount = nonAssigned.size();
         while (nonAssignedCount > 0) {
-            PresetItem nonAssignedPreset = Preset.findBestMatch(getContext(), presets, nonAssigned, regions, elements[0]);
+            PresetItem nonAssignedPreset = Preset.findBestMatch(getContext(), presets, nonAssigned, regions, elements[0], true);
             if (nonAssignedPreset == null) {
                 // no point in continuing
                 break;