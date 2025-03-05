MarcusWolschon/osmeditor4android

commit d2df2be8d617153130e966653416338c64a17ae0

Add UI to select clipboard when pasting

src/main/java/de/blau/android/PoiListAdapter.java
@@ -25,7 +25,6 @@ import de.blau.android.validation.Validator;
 
 public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewHolder> {
 
-    private static final int ICON_SIZE   = 36;
     static final int         ROW_MARGIN  = 20;
     private static final int ROW_PADDING = 5;
 
@@ -37,8 +36,9 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
 
     private int                   width;
     private final OnClickListener onClickListener;
-    private int                   defaultTextColor;
-    private int                   defaultBackgroundColor;
+    private final int             defaultTextColor;
+    private final int             defaultBackgroundColor;
+    private final int             iconSize;
 
     private static final BitmapDrawable NO_ICON = new BitmapDrawable(); // NOSONAR
 
@@ -74,6 +74,8 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
         defaultTextColor = ThemeUtils.getStyleAttribColorValue(ctx, R.attr.textColor, R.color.black);
         defaultBackgroundColor = ThemeUtils.getStyleAttribColorValue(ctx, R.attr.colorSecondary, R.color.ccc_white);
 
+        iconSize = Math.round(ctx.getResources().getDimension(R.dimen.poi_list_icon_size));
+
         onClickListener = clickedView -> {
             OsmElement element = (OsmElement) clickedView.getTag();
             ViewBox box = new ViewBox(element.getBounds());
@@ -100,7 +102,7 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
         tv.setPadding(ROW_PADDING, ROW_PADDING, ROW_PADDING, ROW_PADDING);
         tv.setMaxWidth(width);
         tv.setMinWidth(width);
-        tv.setMinHeight(ICON_SIZE + 2 * ROW_PADDING);
+        tv.setMinHeight(iconSize + 2 * ROW_PADDING);
         tv.setGravity(Gravity.CENTER_VERTICAL);
         tv.setCompoundDrawablePadding(ROW_MARGIN);
         tv.setOnClickListener(onClickListener);
@@ -121,18 +123,7 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
             holder.tv.setBackgroundColor(defaultBackgroundColor);
             holder.tv.setTextColor(defaultTextColor);
         }
-        BitmapDrawable icon = e.getFromCache(iconCache);
-        if (icon == null) {
-            icon = NO_ICON;
-            PresetItem item = Preset.findBestMatch(App.getCurrentPresets(ctx), e.getTags(), null, null);
-            if (item != null) {
-                Drawable tempIcon = item.getIcon(ctx, ICON_SIZE);
-                if (tempIcon instanceof BitmapDrawable) {
-                    icon = (BitmapDrawable) tempIcon;
-                }
-                e.addToCache(iconCache, icon);
-            }
-        }
+        BitmapDrawable icon = getIcon(ctx, iconCache, iconSize, e);
         holder.tv.setCompoundDrawables(icon != NO_ICON ? icon : null, null, null, null);
 
         String description = e.getFromCache(descriptionCache);
@@ -141,6 +132,33 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
             e.addToCache(descriptionCache, description);
         }
         holder.tv.setText(description);
+    }
+
+    /**
+     * Get an icon for an element caching it if necessary
+     * 
+     * @param ctx an Android Context
+     * @param iconCache the cache
+     * @param iconSize the size of the icon
+     * @param e the OsmElement
+     * @return a BitmapDrawable
+     */
+    @NonNull
+    private static BitmapDrawable getIcon(@NonNull Context ctx, @NonNull java.util.Map<java.util.Map<String, String>, BitmapDrawable> iconCache, int iconSize,
+            @NonNull final OsmElement e) {
+        BitmapDrawable icon = e.getFromCache(iconCache);
+        if (icon == null) {
+            icon = NO_ICON;
+            PresetItem item = Preset.findBestMatch(App.getCurrentPresets(ctx), e.getTags(), null, null);
+            if (item != null) {
+                Drawable tempIcon = item.getIcon(ctx, iconSize);
+                if (tempIcon instanceof BitmapDrawable) {
+                    icon = (BitmapDrawable) tempIcon;
+                }
+                e.addToCache(iconCache, icon);
+            }
+        }
+        return icon;
     }
 
     @Override

src/main/java/de/blau/android/Logic.java
@@ -5796,26 +5796,33 @@ public class Logic {
     /**
      * Copy element to clipboard
      * 
+     * @param activity optional activity we were called from
      * @param element element to copy
      */
-    public void copyToClipboard(@NonNull OsmElement element) {
+    public void copyToClipboard(@Nullable FragmentActivity activity, @NonNull OsmElement element) {
         List<OsmElement> list = new ArrayList<>();
         list.add(element);
-        copyToClipboard(list);
+        copyToClipboard(activity, list);
     }
 
     /**
      * Copy elements to clipboard
      * 
+     * @param activity optional activity we were called from
      * @param elements elements to copy
      */
-    public void copyToClipboard(@NonNull List<OsmElement> elements) {
+    public void copyToClipboard(@Nullable FragmentActivity activity, @NonNull List<OsmElement> elements) {
         int[] centroid = calcCentroid(elements);
         if (centroid.length != 2) {
             Log.e(DEBUG_TAG, "Unable to determine centroid");
             return;
         }
-        getDelegator().copyToClipboard(elements, centroid[0], centroid[1]);
+        try {
+            getDelegator().copyToClipboard(elements, centroid[0], centroid[1]);
+        } catch (OsmIllegalOperationException | StorageException ex) {
+            handleDelegatorException(activity, ex);
+            // don't rethrow
+        }
     }
 
     /**
@@ -5824,7 +5831,7 @@ public class Logic {
      * @param activity the activity we were called from
      * @param element element to cut
      */
-    public void cutToClipboard(@Nullable Activity activity, @NonNull OsmElement element) {
+    public void cutToClipboard(@Nullable FragmentActivity activity, @NonNull OsmElement element) {
         List<OsmElement> list = new ArrayList<>();
         list.add(element);
         cutToClipboard(activity, list);
@@ -5836,14 +5843,19 @@ public class Logic {
      * @param activity the activity we were called from
      * @param elements the elements to cut
      */
-    public void cutToClipboard(@Nullable Activity activity, @NonNull List<OsmElement> elements) {
+    public void cutToClipboard(@Nullable FragmentActivity activity, @NonNull List<OsmElement> elements) {
         createCheckpoint(activity, R.string.undo_action_cut);
         int[] centroid = calcCentroid(elements);
         if (centroid.length != 2) {
             Log.e(DEBUG_TAG, "Unable to determine centroid");
             return;
         }
-        getDelegator().cutToClipboard(elements, centroid[0], centroid[1]);
+        try {
+            getDelegator().cutToClipboard(elements, centroid[0], centroid[1]);
+        } catch (OsmIllegalOperationException | StorageException ex) {
+            handleDelegatorException(activity, ex);
+            // don't rethrow
+        }
         invalidateMap();
     }
 

src/main/java/de/blau/android/easyedit/ElementSelectionActionModeCallback.java
@@ -342,7 +342,7 @@ public abstract class ElementSelectionActionModeCallback extends EasyEditActionM
             showHistory();
             break;
         case MENUITEM_COPY:
-            logic.copyToClipboard(element);
+            logic.copyToClipboard(main, element);
             mode.finish();
             break;
         case MENUITEM_CUT:
@@ -631,7 +631,7 @@ public abstract class ElementSelectionActionModeCallback extends EasyEditActionM
     @Override
     public boolean processShortcut(Character c) {
         if (c == Util.getShortCut(main, R.string.shortcut_copy)) {
-            logic.copyToClipboard(element);
+            logic.copyToClipboard(main, element);
             manager.finish();
             return true;
         } else if (c == Util.getShortCut(main, R.string.shortcut_cut)) {

src/main/java/de/blau/android/easyedit/MultiSelectWithGeometryActionModeCallback.java
@@ -196,7 +196,7 @@ public class MultiSelectWithGeometryActionModeCallback extends MultiSelectAction
             menuDelete(false);
             break;
         case ElementSelectionActionModeCallback.MENUITEM_COPY:
-            logic.copyToClipboard(selection);
+            logic.copyToClipboard(main, selection);
             mode.finish();
             break;
         case ElementSelectionActionModeCallback.MENUITEM_CUT:
@@ -457,7 +457,7 @@ public class MultiSelectWithGeometryActionModeCallback extends MultiSelectAction
     @Override
     public boolean processShortcut(Character c) {
         if (c == Util.getShortCut(main, R.string.shortcut_copy)) {
-            logic.copyToClipboard(selection);
+            logic.copyToClipboard(main, selection);
             manager.finish();
             return true;
         } else if (c == Util.getShortCut(main, R.string.shortcut_cut)) {

src/main/java/de/blau/android/easyedit/SimpleActionModeCallback.java
@@ -4,14 +4,21 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.CompoundButton;
+import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.view.ActionMode;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.core.graphics.drawable.DrawableCompat;
 import de.blau.android.App;
 import de.blau.android.Logic;
 import de.blau.android.Main;
@@ -19,11 +26,14 @@ import de.blau.android.R;
 import de.blau.android.exception.OsmIllegalOperationException;
 import de.blau.android.exception.StorageException;
 import de.blau.android.layer.LayerType;
+import de.blau.android.osm.ClipboardStorage;
 import de.blau.android.osm.Node;
 import de.blau.android.osm.OsmElement;
 import de.blau.android.tasks.NoteFragment;
+import de.blau.android.util.NumberDrawable;
 import de.blau.android.util.ScreenMessage;
 import de.blau.android.util.ThemeUtils;
+import de.blau.android.util.collections.MRUList;
 import de.blau.android.voice.Commands;
 
 public class SimpleActionModeCallback extends EasyEditActionModeCallback implements android.view.MenuItem.OnMenuItemClickListener {
@@ -79,7 +89,7 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
             }
 
             @Override
-            public void addMenuItems(Context ctx, Menu menu) {
+            public void addMenuItems(EasyEditManager manager, Context ctx, Menu menu) {
                 boolean snap = App.getLogic().getPrefs().isWaySnapEnabled();
                 PathCreationActionModeCallback.addSnapCheckBox(ctx, menu, snap,
                         (CompoundButton buttonView, boolean isChecked) -> App.getLogic().getPrefs().enableWaySnap(isChecked));
@@ -166,6 +176,12 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
             public boolean isEnabled() {
                 return !App.getLogic().clipboardIsEmpty();
             }
+
+            @Override
+            public void addMenuItems(@NonNull EasyEditManager manager, @NonNull Context ctx, @NonNull Menu menu) {
+                setUpClipboardButtons(manager, ctx, menu);
+            }
+
         },
         /**
          * Paste an object from the clipboard, without exiting the action mode
@@ -174,7 +190,12 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
                 (main, manager, x, y) -> App.getLogic().pasteFromClipboard(main, 0, x, y)) {
             @Override
             public boolean isEnabled() {
-                return !App.getLogic().clipboardIsEmpty() && !App.getDelegator().clipboardContentWasCut();
+                return !App.getLogic().clipboardIsEmpty();
+            }
+
+            @Override
+            public void addMenuItems(@NonNull EasyEditManager manager, @NonNull Context ctx, @NonNull Menu menu) {
+                setUpClipboardButtons(manager, ctx, menu);
             }
         };
 
@@ -224,7 +245,7 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
          * @param x screen x coordinate
          * @param y screen y coordinate
          */
-        public void execute(final Main main, final EasyEditManager manager, final float x, final float y) {
+        public void execute(@NonNull final Main main, @NonNull final EasyEditManager manager, final float x, final float y) {
             actionCallback.action(main, manager, x, y);
         }
 
@@ -240,10 +261,11 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
         /**
          * Add one or more menu items to the initial menu
          * 
+         * @param manager the current EasyEditManager
          * @param ctx an Android Context
          * @param menu the Menu we append the items too
          */
-        public void addMenuItems(@NonNull Context ctx, @NonNull Menu menu) {
+        public void addMenuItems(@NonNull final EasyEditManager manager, @NonNull Context ctx, @NonNull Menu menu) {
             // nothing
         }
     }
@@ -259,6 +281,10 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
     public SimpleActionModeCallback(@NonNull EasyEditManager manager, @NonNull SimpleAction simpleMode) {
         super(manager);
         this.simpleAction = simpleMode;
+    }
+
+    public void invalidate() {
+        manager.invalidate();
     }
 
     @Override
@@ -280,7 +306,7 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
         super.onPrepareActionMode(mode, menu);
         menu.clear();
         menuUtil.reset();
-        simpleAction.addMenuItems(main, menu);
+        simpleAction.addMenuItems(manager, main, menu);
         menu.add(GROUP_BASE, MENUITEM_HELP, Menu.CATEGORY_SYSTEM | 10, R.string.menu_help).setIcon(ThemeUtils.getResIdFromAttribute(main, R.attr.menu_help));
         arrangeMenu(menu);
         return true;
@@ -348,6 +374,68 @@ public class SimpleActionModeCallback extends EasyEditActionModeCallback impleme
             }
         } else {
             manager.finish();
+        }
+    }
+
+    /**
+     * Set up the buttons representing clipboards
+     * 
+     * @param manager the current EasyEditManager instance
+     * @param ctx an Android Context
+     * @param menu the Menu to add the buttons to
+     */
+    private static void setUpClipboardButtons(@NonNull EasyEditManager manager, @NonNull Context ctx, @NonNull Menu menu) {
+        final List<ClipboardStorage> clipboards = App.getDelegator().getClipboards();
+        final Drawable bgEmpty = AppCompatResources.getDrawable(ctx, R.drawable.clipboard_bg);
+        final Drawable bgOrange = AppCompatResources.getDrawable(ctx, R.drawable.clipboard_bg_orange);
+        int count = 0;
+        for (ClipboardStorage clipboard : clipboards) {
+            final int c = count;
+            MenuItem item = menu.add(Integer.toString(count + 1)).setOnMenuItemClickListener((MenuItem menuItem) -> {
+                for (int j = 0; j < clipboards.size(); j++) {
+                    MenuItem mi = menu.getItem(j);
+                    setIconBackground(mi, bgEmpty);
+                }
+                setIconBackground(menuItem, bgOrange);
+                ((MRUList<ClipboardStorage>) clipboards).push(clipboards.get(c));
+                manager.invalidate();
+                return true;
+            });
+            Drawable icon = clipboard.getIcon(ctx);
+            if (icon == ClipboardStorage.NO_ICON) {
+                icon = new NumberDrawable(ctx);
+                ((NumberDrawable) icon).setNumber(count + 1);
+            }
+            LayerDrawable layerDrawable = new LayerDrawable(new Drawable[] { bgEmpty });
+            layerDrawable.setId(0, 0);
+            layerDrawable.setLayerGravity(0, Gravity.CENTER);
+            layerDrawable.addLayer(icon);
+            layerDrawable.setLayerGravity(1, Gravity.CENTER);
+            layerDrawable.setLayerInsetLeft(1, 6);
+            layerDrawable.setLayerInsetRight(1, 6);
+            layerDrawable.setLayerInsetTop(1, 6);
+            layerDrawable.setLayerInsetBottom(1, 6);
+            item.setIcon(layerDrawable);
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+            if (count == 0) {
+                setIconBackground(item, bgOrange);
+            }
+            count++;
+        }
+    }
+
+    /**
+     * Set the background image for a clipboard button
+     * 
+     * @param item the MenuItem
+     * @param bg the background Drawable
+     */
+    private static void setIconBackground(@NonNull MenuItem item, @NonNull Drawable bg) {
+        LayerDrawable drawable = (LayerDrawable) item.getIcon();
+        if (drawable != null) {
+            drawable.setDrawableByLayerId(0, bg);
+            drawable.invalidateSelf();
         }
     }
 }

src/main/java/de/blau/android/osm/StorageDelegator.java
@@ -2583,26 +2583,27 @@ public class StorageDelegator implements Serializable, Exportable, DataStorage {
                     throw new IllegalArgumentException("Cutting of Relations not supported");
                 }
                 toCut.add(e);
-                if (e instanceof Way) {
-                    undo.save(e);
-                    // clone all nodes that are members of other ways that are not being cut
-                    List<Node> nodes = new ArrayList<>(((Way) e).getNodes());
-                    for (Node nd : nodes) {
-                        List<Way> ways = currentStorage.getWays(nd);
-                        if (ways.size() <= 1) { // 1 is expected (our way will be deleted later)
-                            continue;
-                        }
-                        Node newNode = replacedNodes.get(nd.getOsmId());
-                        if (newNode == null) {
-                            // check if there is actually a Way we are not cutting
-                            for (Way w : ways) {
-                                if (!elements.contains(w)) {
-                                    newNode = factory.createNodeWithNewId(nd.getLat(), nd.getLon());
-                                    newNode.setTags(nd.getTags());
-                                    insertElementSafe(newNode);
-                                    replacedNodes.put(nd.getOsmId(), newNode);
-                                    break;
-                                }
+                if (!(e instanceof Way)) {
+                    continue;
+                }
+                undo.save(e);
+                // clone all nodes that are members of other ways that are not being cut
+                List<Node> nodes = new ArrayList<>(((Way) e).getNodes());
+                for (Node nd : nodes) {
+                    List<Way> ways = currentStorage.getWays(nd);
+                    if (ways.size() <= 1) { // 1 is expected (our way will be deleted later)
+                        continue;
+                    }
+                    Node newNode = replacedNodes.get(nd.getOsmId());
+                    if (newNode == null) {
+                        // check if there is actually a Way we are not cutting
+                        for (Way w : ways) {
+                            if (!elements.contains(w)) {
+                                newNode = factory.createNodeWithNewId(nd.getLat(), nd.getLon());
+                                newNode.setTags(nd.getTags());
+                                insertElementSafe(newNode);
+                                replacedNodes.put(nd.getOsmId(), newNode);
+                                break;
                             }
                         }
                     }
@@ -2892,16 +2893,6 @@ public class StorageDelegator implements Serializable, Exportable, DataStorage {
     public boolean clipboardIsEmpty() {
         ClipboardStorage last = clipboards.last();
         return last == null || last.isEmpty();
-    }
-
-    /**
-     * Check if the content of the current clipboard was cut
-     * 
-     * @return true if the current clipboards content was cut
-     */
-    public boolean clipboardContentWasCut() {
-        ClipboardStorage last = clipboards.last();
-        return last != null && last.contentsWasCut();
     }
 
     /**
@@ -4252,6 +4243,16 @@ public class StorageDelegator implements Serializable, Exportable, DataStorage {
     @NonNull
     public BoundingBox getLastBox() {
         return currentStorage.getLastBox();
+    }
+
+    /**
+     * Get the list of clipboards
+     * 
+     * @return the clipboards
+     */
+    @NonNull
+    public List<ClipboardStorage> getClipboards() {
+        return clipboards;
     }
 
     /**

src/main/java/de/blau/android/util/BadgeDrawable.java
@@ -4,9 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
@@ -23,7 +21,7 @@ import de.blau.android.R;
  * @author simon
  *
  */
-public class BadgeDrawable extends Drawable {
+public class BadgeDrawable extends BaseDrawable {
 
     private static final int MAX      = 1000;
     private Paint            mBadgePaint;
@@ -120,21 +118,6 @@ public class BadgeDrawable extends Drawable {
         // Only draw a badge if there are notifications.
         mWillDraw = count > 0;
         invalidateSelf();
-    }
-
-    @Override
-    public void setAlpha(int alpha) {
-        // do nothing
-    }
-
-    @Override
-    public void setColorFilter(ColorFilter cf) {
-        // do nothing
-    }
-
-    @Override
-    public int getOpacity() {
-        return PixelFormat.UNKNOWN;
     }
 
     /**

src/main/java/de/blau/android/util/BaseDrawable.java
@@ -1,0 +1,29 @@
+package de.blau.android.util;
+
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+
+/**
+ * A base drawable class
+ * 
+ * @author simon
+ *
+ */
+public abstract class BaseDrawable extends Drawable {
+
+    @Override
+    public void setAlpha(int alpha) {
+        // do nothing
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+        // do nothing
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.UNKNOWN;
+    }
+}

commit 754ddc70d20b3febe52897d0e920d5124f9c7f8b

Fix size of icons in nearby poi display
Resolves regression from
https://github.com/MarcusWolschon/osmeditor4android/commit/d2df2be8d617153130e966653416338c64a17ae0

src/main/java/de/blau/android/PoiListAdapter.java
@@ -20,6 +20,7 @@ import de.blau.android.osm.ViewBox;
 import de.blau.android.presets.Preset;
 import de.blau.android.presets.PresetItem;
 import de.blau.android.resources.DataStyle.FeatureStyle;
+import de.blau.android.util.Density;
 import de.blau.android.util.ThemeUtils;
 import de.blau.android.validation.Validator;
 
@@ -74,7 +75,8 @@ public class PoiListAdapter extends RecyclerView.Adapter<PoiListAdapter.PoiViewH
         defaultTextColor = ThemeUtils.getStyleAttribColorValue(ctx, R.attr.textColor, R.color.black);
         defaultBackgroundColor = ThemeUtils.getStyleAttribColorValue(ctx, R.attr.colorSecondary, R.color.ccc_white);
 
-        iconSize = Math.round(ctx.getResources().getDimension(R.dimen.poi_list_icon_size));
+        // note currently the rendering code assumes that the dimensions are in DP not PX
+        iconSize = Density.pxToDp(ctx, Math.round(ctx.getResources().getDimension(R.dimen.poi_list_icon_size)));
 
         onClickListener = clickedView -> {
             OsmElement element = (OsmElement) clickedView.getTag();