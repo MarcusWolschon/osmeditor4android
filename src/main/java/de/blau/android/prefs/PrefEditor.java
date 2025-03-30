bb6ad883837752344de14b93d9d226b9ff3f4c58
simonpoole <simon@poole.ch>
2025-03-30 17:07:48 +0200

    Start pref activity with fragment context
    It is unclear if this is a behaviour change or never worked, in any case
    we now start the preference activity with the fragment context so that
    onActivityResult in the fragment actually gets called.
    Fixes https://github.com/MarcusWolschon/osmeditor4android/issues/2848

--- src/main/java/de/blau/android/prefs/PrefEditor.java
+++ src/main/java/de/blau/android/prefs/PrefEditor.java
@@ -1,11 +1,14 @@
 package de.blau.android.prefs;
 
+import static de.blau.android.contract.Constants.LOG_TAG_LEN;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import de.blau.android.R;
 
 /**
@@ -15,7 +18,8 @@ import de.blau.android.R;
  */
 public class PrefEditor extends PrefEditorActivity {
 
-    private static final String DEBUG_TAG = PrefEditor.class.getSimpleName().substring(0, Math.min(23, PrefEditor.class.getSimpleName().length()));
+    private static final int    TAG_LEN   = Math.min(LOG_TAG_LEN, PrefEditor.class.getSimpleName().length());
+    private static final String DEBUG_TAG = PrefEditor.class.getSimpleName().substring(0, TAG_LEN);
 
     /**
      * Start the PrefEditor activity
@@ -36,6 +40,17 @@ public class PrefEditor extends PrefEditorActivity {
     public static void start(@NonNull Activity activity, int requestCode) {
         Intent intent = new Intent(activity, PrefEditor.class);
         activity.startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * Start the PrefEditor activity and wait for a result
+     * 
+     * @param fragment the calling fragment
+     * @param requestCode an int value to identify the request
+     */
+    public static void start(@NonNull Fragment fragment, int requestCode) {
+        Intent intent = new Intent(fragment.getContext(), PrefEditor.class);
+        fragment.startActivityForResult(intent, requestCode);
     }
 
     @Override
