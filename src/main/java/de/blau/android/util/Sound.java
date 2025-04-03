3b7997903b592dc0f198fca3c78feef5267b4b2d
simonpoole <simon@poole.ch>
2025-04-03 10:20:40 +0200

    Run beeping in a thread
    This might or might not stop some devices from ANRing

--- src/main/java/de/blau/android/util/Sound.java
+++ src/main/java/de/blau/android/util/Sound.java
@@ -1,18 +1,24 @@
 package de.blau.android.util;
 
+import static de.blau.android.contract.Constants.LOG_TAG_LEN;
+
+import java.util.concurrent.locks.ReentrantLock;
+
 import android.media.AudioManager;
 import android.media.ToneGenerator;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import de.blau.android.App;
 import de.blau.android.prefs.Preferences;
 
 public final class Sound {
-    private static final String DEBUG_TAG = Sound.class.getSimpleName().substring(0, Math.min(23, Sound.class.getSimpleName().length()));
+
+    private static final int    TAG_LEN   = Math.min(LOG_TAG_LEN, Sound.class.getSimpleName().length());
+    private static final String DEBUG_TAG = Sound.class.getSimpleName().substring(0, TAG_LEN);
 
     private static final int BEEP_DURATION       = 200;
     public static final int  BEEP_DEFAULT_VOLUME = 50;
+
+    private static final ReentrantLock lock = new ReentrantLock();
 
     /**
      * Private constructor to prevent instantiation
@@ -25,22 +31,36 @@ public final class Sound {
      * Beep
      */
     public static void beep() {
-        Preferences prefs = App.getLogic().getPrefs();
-        int volume = BEEP_DEFAULT_VOLUME;
-        if (prefs != null) {
-            volume = prefs.getBeepVolume();
-        }
-        try {
-            ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_ALARM, volume);
-            toneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, BEEP_DURATION);
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.postDelayed(() -> {
-                if (toneGenerator != null) {
-                    toneGenerator.release();
+        new ExecutorTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void v) {
+                try {
+                    if (!lock.tryLock()) {
+                        Log.w(DEBUG_TAG, "beep locked");
+                        return null;
+                    }
+                    Preferences prefs = App.getLogic().getPrefs();
+                    int volume = BEEP_DEFAULT_VOLUME;
+                    if (prefs != null) {
+                        volume = prefs.getBeepVolume();
+                        if (volume == 0) {
+                            return null; // turned off
+                        }
+                    }
+                    try {
+                        ToneGenerator toneGenerator = new ToneGenerator(AudioManager.STREAM_ALARM, volume);
+                        toneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, BEEP_DURATION);
+                        toneGenerator.release();
+                    } catch (Exception ex) { // NOSONAR
+                        Log.e(DEBUG_TAG, "beep failed with " + ex.getMessage());
+                    }
+                } finally {
+                    if (lock.isHeldByCurrentThread()) {
+                        lock.unlock();
+                    }
                 }
-            }, BEEP_DURATION + 50L);
-        } catch (RuntimeException rex) { // NOSONAR
-            Log.e(DEBUG_TAG, "beep failed with " + rex.getMessage());
-        }
+                return null;
+            }
+        }.execute();
     }
 }
