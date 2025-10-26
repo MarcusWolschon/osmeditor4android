80a47935992d7a50f6b83af45a5e3ecff7305a2e
simonpoole <simon@poole.ch>
2025-10-26 22:18:32 +0100

    Organize imports

--- src/main/java/de/blau/android/easyedit/NodeSelectionActionModeCallback.java
+++ src/main/java/de/blau/android/easyedit/NodeSelectionActionModeCallback.java
@@ -15,7 +15,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
