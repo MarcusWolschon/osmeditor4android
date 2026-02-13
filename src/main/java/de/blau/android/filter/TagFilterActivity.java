e5ae4b0555d1d9e496c63d8af866aa8cda9a76cf
simonpoole <simon@poole.ch>
2026-02-13 17:08:54 +0100

    Remove unused import

--- src/main/java/de/blau/android/filter/TagFilterActivity.java
+++ src/main/java/de/blau/android/filter/TagFilterActivity.java
@@ -8,7 +8,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
