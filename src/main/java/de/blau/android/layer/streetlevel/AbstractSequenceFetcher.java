MarcusWolschon/osmeditor4android

commit d1493030ed12f1dc5e123f1440164cb54df13de9

Replace deprecated URL(string) constructor with URI(string).toURL()

src/main/java/de/blau/android/layer/streetlevel/AbstractSequenceFetcher.java
@@ -4,6 +4,8 @@ import static de.blau.android.contract.Constants.LOG_TAG_LEN;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -51,14 +53,14 @@ public abstract class AbstractSequenceFetcher implements Runnable {
     @Override
     public void run() {
         try {
-            URL url = new URL(String.format(urlTemplate, sequenceId, apiKey));
+            URL url = new URI(String.format(urlTemplate, sequenceId, apiKey)).toURL();
             ArrayList<String> ids = new ArrayList<>();
             do {
                 Log.d(DEBUG_TAG, "query sequence: " + url.toString());
                 url = querySequence(url, ids);
             } while (url != null);
             saveIdsAndUpdate(ids);
-        } catch (IOException ex) {
+        } catch (IOException | URISyntaxException ex) {
             Log.e(DEBUG_TAG, "query sequence failed with " + ex.getMessage());
             // don't (re-)throw an exception as that will crash the app it seems
         }
@@ -69,9 +71,10 @@ public abstract class AbstractSequenceFetcher implements Runnable {
      * 
      * @param url the URL
      * @throws IOException if IO goes wrong
+     * @throws URISyntaxException if the URI couldn't be parsed
      */
     @Nullable
-    protected URL querySequence(@NonNull URL url, @NonNull ArrayList<String> ids) throws IOException {
+    protected URL querySequence(@NonNull URL url, @NonNull ArrayList<String> ids) throws IOException, URISyntaxException {
         Request request = new Request.Builder().url(url).build();
         OkHttpClient client = App.getHttpClient().newBuilder().connectTimeout(20000, TimeUnit.MILLISECONDS).readTimeout(20000, TimeUnit.MILLISECONDS).build();
         Call call = client.newCall(request);
@@ -116,7 +119,8 @@ public abstract class AbstractSequenceFetcher implements Runnable {
      * @param ids
      * @return a List of ids
      * @throws IOException if the ids can't be found
+     * @throws URISyntaxException on URI parsing errors
      */
     @Nullable
-    protected abstract URL getIds(@NonNull JsonElement root, ArrayList<String> ids) throws IOException;
+    protected abstract URL getIds(@NonNull JsonElement root, ArrayList<String> ids) throws IOException, URISyntaxException;
 }

src/main/java/de/blau/android/layer/streetlevel/mapillary/MapillaryLoader.java
@@ -7,7 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -60,7 +61,7 @@ class MapillaryLoader extends NetworkImageLoader {
         return () -> {
             Log.d(DEBUG_TAG, "querying mapillary server for " + key);
             try {
-                Request request = new Request.Builder().url(new URL(String.format(imageUrl, key))).build();
+                Request request = new Request.Builder().url(new URI(String.format(imageUrl, key)).toURL()).build();
                 OkHttpClient client = App.getHttpClient().newBuilder().connectTimeout(20000, TimeUnit.MILLISECONDS).readTimeout(20000, TimeUnit.MILLISECONDS)
                         .build();
                 Call mapillaryCall = client.newCall(request);
@@ -80,7 +81,7 @@ class MapillaryLoader extends NetworkImageLoader {
                 }
                 setImage(view, imageFile);
                 pruneCache();
-            } catch (IOException e) {
+            } catch (IOException | URISyntaxException e) {
                 Log.e(DEBUG_TAG, e.getMessage());
             }
         };

src/main/java/de/blau/android/layer/streetlevel/panoramax/PanoramaxOverlay.java
@@ -4,6 +4,8 @@ import static de.blau.android.contract.Constants.LOG_TAG_LEN;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -312,7 +314,7 @@ public class PanoramaxOverlay extends AbstractImageOverlay {
         }
 
         @Override
-        protected URL getIds(JsonElement root, ArrayList<String> ids) throws IOException {
+        protected URL getIds(JsonElement root, ArrayList<String> ids) throws IOException, URISyntaxException {
             JsonElement features = ((JsonObject) root).get(FEATURES_KEY);
             if (!(features instanceof JsonArray)) {
                 throw new IOException("features not a JsonArray");
@@ -330,7 +332,7 @@ public class PanoramaxOverlay extends AbstractImageOverlay {
                     if (element instanceof JsonObject && ((JsonObject) element).has(REL_KEY)
                             && NEXT_VALUE.equals(((JsonObject) element).get(REL_KEY).getAsString())) {
                         Log.d(DEBUG_TAG, "get next page");
-                        return new URL(((JsonObject) element).get(HREF_KEY).getAsString());
+                        return new URI(((JsonObject) element).get(HREF_KEY).getAsString()).toURL();
                     }
                 }
             }

src/androidTest/java/de/blau/android/resources/LayerSourceTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -135,7 +137,7 @@ public class LayerSourceTest {
                 URL url = null;
                 try {
                     // remove query
-                    URL input = new URL(inputString);
+                    URL input = new URI(inputString).toURL();
                     url = new URL(input.getProtocol(), input.getHost(), input.getPort(), "");
                     Result cached = done.get(url);
                     if (cached != null) {
@@ -163,7 +165,7 @@ public class LayerSourceTest {
                     Result result = new Result(CheckStatus.DOESNTEXIST, inputString);
                     done.put(url, result);
                     return result;
-                } catch (IOException e) {
+                } catch (IOException | URISyntaxException e) {
                     if (e instanceof javax.net.ssl.SSLHandshakeException) {
                         Log.d(DEBUG_TAG, "Cause: " + ((javax.net.ssl.SSLHandshakeException) e).getCause());
                         Result result = new Result(CheckStatus.SSLERROR, inputString);

src/androidTest/java/de/blau/android/resources/OAMTest.java
@@ -1,6 +1,7 @@
 package de.blau.android.resources;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.junit.After;
@@ -96,7 +97,7 @@ public class OAMTest {
         List<LayerEntry> list = null;
         try {
             list = catalog.getEntries(main, mockServerString, new BoundingBox(8.3879800D, 47.3892400D, 8.3844600D, 47.3911300D));
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             Assert.fail(e.getMessage());
         }
         Assert.assertNotNull(list);
@@ -121,7 +122,7 @@ public class OAMTest {
         List<LayerEntry> list = null;
         try {
             list = catalog.getEntries(main, mockServerString, new BoundingBox(8.3879800D, 47.3892400D, 8.3844600D, 47.3911300D));
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             Assert.fail(e.getMessage());
         }
         Assert.assertNotNull(list);

src/main/java/de/blau/android/Feedback.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
@@ -499,12 +501,12 @@ public class Feedback extends AppCompatActivity implements ActivityResultHandler
     @NonNull
     private static String getIssueLink(@NonNull String repoUser, @NonNull String repoName, @NonNull String url) {
         try {
-            URL issue = new URL(url);
+            URL issue = new URI(url).toURL();
             String[] segments = issue.getPath().split("/");
             if (segments.length > 0) {
                 return Github.getIssueUrl(repoUser, repoName, segments[segments.length - 1]);
             }
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException | URISyntaxException e) {
             // fall through
         }
         Log.e(DEBUG_TAG, "Unparseable issue response " + url);

src/main/java/de/blau/android/Logic.java
@@ -13,6 +13,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.SocketTimeoutException;
+import java.net.URISyntaxException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -3714,7 +3715,7 @@ public class Logic {
             throw new OsmServerException(ErrorCodes.INVALID_DATA_RECEIVED, ex.getLocalizedMessage());
         } catch (OsmServerException ex) {
             throw ex;
-        } catch (IOException ex) {
+        } catch (IOException | URISyntaxException ex) {
             Log.e(DEBUG_TAG, "getElementWithDeleted no connection", ex);
             throw new OsmServerException(ErrorCodes.NO_CONNECTION, ex.getLocalizedMessage());
         }
@@ -3782,7 +3783,7 @@ public class Logic {
                     throw new OsmServerException(ErrorCodes.INVALID_DATA_RECEIVED, ex.getLocalizedMessage());
                 } catch (OsmServerException osex) {
                     throw osex;
-                } catch (IOException ex) {
+                } catch (IOException | URISyntaxException ex) {
                     Log.e(DEBUG_TAG, "getElementsWithDeleted no connection", ex);
                     throw new OsmServerException(ErrorCodes.NO_CONNECTION, ex.getLocalizedMessage());
                 } catch (Exception ex) {
@@ -3812,10 +3813,11 @@ public class Logic {
      * @throws SAXException parsing error
      * @throws IOException if we can't download the nodes
      * @throws ParserConfigurationException parsing error
+     * @throws URISyntaxException if the url couldn't be parsed
      */
     @NonNull
     private Storage multiFetch(@NonNull final Context ctx, @NonNull final Server server, @NonNull OsmParser osmParser, @NonNull String type,
-            @NonNull long[] ids) throws SAXException, IOException, ParserConfigurationException {
+            @NonNull long[] ids) throws SAXException, IOException, ParserConfigurationException, URISyntaxException {
         int end = 0;
         for (int start = 0; start < Math.min(start + Server.MULTI_FETCH_MAX_ELEMENTS, ids.length); start = end) {
             end = start + Math.min(Server.MULTI_FETCH_MAX_ELEMENTS, ids.length - start);
@@ -3837,9 +3839,10 @@ public class Logic {
      * @throws SAXException parsing error
      * @throws IOException if we can't download the nodes
      * @throws ParserConfigurationException parsing error
+     * @throws URISyntaxException if the url couldn't be parsed
      */
     private void downloadMissingWayNodes(@NonNull final Context ctx, @NonNull final Server server, @NonNull final OsmParser osmParser, @Nullable OsmElement way)
-            throws SAXException, IOException, ParserConfigurationException {
+            throws SAXException, IOException, ParserConfigurationException, URISyntaxException {
         // as the API doesn't return way nodes for this call we need to patch things up here
         if (way == null) {
             throw new OsmServerException(ErrorCodes.NOT_FOUND, "downloadMissingWayNodes null way");
@@ -3975,7 +3978,7 @@ public class Logic {
         } catch (OsmServerException e) {
             result = e.getHttpErrorCode();
             Log.e(DEBUG_TAG, "downloadElement problem downloading", e);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             result = ErrorCodes.NO_CONNECTION;
             Log.e(DEBUG_TAG, "downloadElement no connection", e);
         }
@@ -4060,7 +4063,7 @@ public class Logic {
                     return new AsyncResult(ErrorCodes.UNKNOWN_ERROR, e.getMessageWithDescription());
                 } catch (DataConflictException dce) {
                     return new AsyncResult(ErrorCodes.DATA_CONFLICT);
-                } catch (IOException e) {
+                } catch (IOException | URISyntaxException e) {
                     Log.e(DEBUG_TAG, "downloadElements problem downloading", e);
                     return new AsyncResult(ErrorCodes.NO_CONNECTION);
                 }
@@ -4447,7 +4450,7 @@ public class Logic {
                         TransferTasks.merge(context, App.getTaskStorage(), notes);
                         TransferTasks.addBoundingBoxFromData(App.getTaskStorage(), notes);
                     }
-                } catch (UnsupportedFormatException | IOException | SAXException | ParserConfigurationException e) {
+                } catch (UnsupportedFormatException | IOException | SAXException | ParserConfigurationException | URISyntaxException e) {
                     Log.e(DEBUG_TAG, "Problem parsing OSC ", e);
                     return new AsyncResult(ErrorCodes.INVALID_DATA_READ, e.getMessage());
                 } catch (IllegalStateException iex) {
@@ -4853,7 +4856,7 @@ public class Logic {
                     Log.e(DEBUG_TAG, METHOD_UPLOAD, e);
                     result.setError(ErrorCodes.UPLOAD_INCOMPLETE);
                     result.setMessage(e.getLocalizedMessage());
-                } catch (final IOException | NumberFormatException e) {
+                } catch (final IOException | NumberFormatException | URISyntaxException e) {
                     Log.e(DEBUG_TAG, METHOD_UPLOAD, e);
                     result.setError(ErrorCodes.UPLOAD_PROBLEM);
                     result.setMessage(e.getLocalizedMessage());
@@ -5038,7 +5041,7 @@ public class Logic {
                     result.setHttpError(errorCode);
                     result.setMessage(e.getMessage());
                     Logic.mapErrorCode(errorCode, result);
-                } catch (final IOException e) {
+                } catch (final IOException | URISyntaxException e) {
                     result.setError(ErrorCodes.NO_CONNECTION);
                     Log.e(DEBUG_TAG, "", e);
                 } catch (IllegalArgumentException | IllegalStateException | NullPointerException e) {

src/main/java/de/blau/android/Main.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
@@ -1224,7 +1225,7 @@ public class Main extends ConfigurationChangeAwareActivity
     private void displayNote(@NonNull final Context ctx, @NonNull final Logic logic, @NonNull final long id) {
         new ExecutorTask<Long, Void, Note>(logic.getExecutorService(), logic.getHandler()) {
             @Override
-            protected Note doInBackground(Long id) throws NumberFormatException, XmlPullParserException, IOException {
+            protected Note doInBackground(Long id) throws NumberFormatException, XmlPullParserException, IOException, URISyntaxException {
                 TaskStorage storage = App.getTaskStorage();
                 Note note = storage.getNote(id);
                 if (note == null) {
@@ -2320,7 +2321,7 @@ public class Main extends ConfigurationChangeAwareActivity
                     protected Void doInBackground(Void param) {
                         try {
                             server.closeChangeset();
-                        } catch (IOException e) {
+                        } catch (IOException | URISyntaxException e) {
                             // Never fail
                         }
                         return null;

commit fbe7ddb692be4513f851556d747e293abbba0404

Mapillary support broke with d1493030ed12f1dc5e123f1440164cb54df13de9
Mapillarys api key contain unsafe characters that are rejected by URI
but not by URL. Unluckily URL encoding causes issues with String.format
so there is currently no really better solution to go back to URL in
some cases.

src/main/java/de/blau/android/layer/streetlevel/AbstractSequenceFetcher.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +20,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 import de.blau.android.App;
 import de.blau.android.contract.MimeTypes;
+import de.blau.android.osm.OsmXml;
 import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -53,7 +55,7 @@ public abstract class AbstractSequenceFetcher implements Runnable {
     @Override
     public void run() {
         try {
-            URL url = new URI(String.format(urlTemplate, sequenceId, apiKey)).toURL();
+            URL url = new URI(String.format(urlTemplate, sequenceId, apiKey != null ? URLEncoder.encode(apiKey, OsmXml.UTF_8) : null)).toURL();
             ArrayList<String> ids = new ArrayList<>();
             do {
                 Log.d(DEBUG_TAG, "query sequence: " + url.toString());

src/main/java/de/blau/android/layer/streetlevel/mapillary/MapillaryLoader.java
@@ -7,8 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -33,6 +32,7 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 class MapillaryLoader extends NetworkImageLoader {
+
     private static final long serialVersionUID = 2L;
 
     private static final int      TAG_LEN   = Math.min(LOG_TAG_LEN, MapillaryLoader.class.getSimpleName().length());
@@ -43,6 +43,7 @@ class MapillaryLoader extends NetworkImageLoader {
     private static final String COMPUTED_COMPASS_ANGLE_FIELD = "computed_compass_angle";
     private static final String CAPTURED_AT_FIELD            = "captured_at";
     private static final String THUMB_2048_URL_FIELD         = "thumb_2048_url";
+    private static final int    TIMEOUT                      = 20000;
 
     /**
      * Construct a new loader
@@ -61,8 +62,8 @@ class MapillaryLoader extends NetworkImageLoader {
         return () -> {
             Log.d(DEBUG_TAG, "querying mapillary server for " + key);
             try {
-                Request request = new Request.Builder().url(new URI(String.format(imageUrl, key)).toURL()).build();
-                OkHttpClient client = App.getHttpClient().newBuilder().connectTimeout(20000, TimeUnit.MILLISECONDS).readTimeout(20000, TimeUnit.MILLISECONDS)
+                Request request = new Request.Builder().url(new URL(String.format(imageUrl, key))).build();
+                OkHttpClient client = App.getHttpClient().newBuilder().connectTimeout(TIMEOUT, TimeUnit.MILLISECONDS).readTimeout(20000, TimeUnit.MILLISECONDS)
                         .build();
                 Call mapillaryCall = client.newCall(request);
                 Response mapillaryCallResponse = mapillaryCall.execute();
@@ -81,7 +82,7 @@ class MapillaryLoader extends NetworkImageLoader {
                 }
                 setImage(view, imageFile);
                 pruneCache();
-            } catch (IOException | URISyntaxException e) {
+            } catch (IOException e) {
                 Log.e(DEBUG_TAG, e.getMessage());
             }
         };

src/main/java/de/blau/android/layer/streetlevel/mapillary/MapillaryOverlay.java
@@ -131,28 +131,28 @@ public class MapillaryOverlay extends AbstractImageOverlay {
 
     @Override
     public void onSelected(FragmentActivity activity, de.blau.android.util.mvt.VectorTileDecoder.Feature f) {
-        if (isPoint(f)) {
-            // we ignore anything except the images for now
-            java.util.Map<String, Object> attributes = f.getAttributes();
-            String sequenceId = (String) attributes.get(SEQUENCE_ID_KEY);
-            Long id = (Long) attributes.get(ID_KEY);
-            if (id != null && sequenceId != null) {
-                ArrayList<String> keys = mapillaryState != null ? mapillaryState.sequenceCache.get(sequenceId) : null;
-                if (keys == null) {
-                    try {
-                        Thread t = new Thread(null, new MapillarySequenceFetcher(activity, mapillarySequencesUrl, sequenceId, id, apiKey),
-                                "Mapillary Sequence");
-                        t.start();
-                    } catch (SecurityException | IllegalThreadStateException | IllegalStateException e) {
-                        Log.e(DEBUG_TAG, "Unable to run SequenceFetcher " + e.getMessage());
-                        return;
-                    }
-                } else {
-                    showImages(activity, id, keys);
+        if (!isPoint(f)) {
+            return;
+        }
+        // we ignore anything except the images for now
+        java.util.Map<String, Object> attributes = f.getAttributes();
+        String sequenceId = (String) attributes.get(SEQUENCE_ID_KEY);
+        Long id = (Long) attributes.get(ID_KEY);
+        if (id != null && sequenceId != null) {
+            ArrayList<String> keys = mapillaryState != null ? mapillaryState.sequenceCache.get(sequenceId) : null;
+            if (keys == null) {
+                try {
+                    Thread t = new Thread(null, new MapillarySequenceFetcher(activity, mapillarySequencesUrl, sequenceId, id, apiKey), "Mapillary Sequence");
+                    t.start();
+                } catch (SecurityException | IllegalThreadStateException | IllegalStateException e) {
+                    Log.e(DEBUG_TAG, "Unable to run SequenceFetcher " + e.getMessage());
+                    return;
                 }
-                setSelected(f);
-                mapillaryState.sequenceId = sequenceId;
+            } else {
+                showImages(activity, id, keys);
             }
+            setSelected(f);
+            mapillaryState.sequenceId = sequenceId;
         }
     }
 

src/main/java/de/blau/android/layer/streetlevel/panoramax/PanoramaxOverlay.java
@@ -370,7 +370,6 @@ public class PanoramaxOverlay extends AbstractImageOverlay {
                 return;
             }
             urls.put(idString, href.getAsString());
-
         }
     }
 